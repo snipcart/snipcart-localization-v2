2a9416ccfa659a6c693f659b8dbc5850041a9ae2
Antoine Leclair <antoineleclair@gmail.com>
2013-09-03 22:12:04 -0400

    Add EN and FR locales

---
+++ locales/fr.js
@@ -0,0 +1,250 @@
+﻿var SnipcartSettings = SnipcartSettings || {};
+SnipcartSettings.locales = SnipcartSettings.locales || {};
+SnipcartSettings.locales['fr'] = {
+yes:
+"Oui",
+no:
+"Non",
+print:
+"Imprimer",
+download_as_pdf:
+"Télécharger le PDF",
+checkout:
+"Passer au paiement",
+close:
+"Fermer",
+name:
+"Nom",
+company_name:
+"Nom de la compagnie",
+share_by_email:
+"Partager par courriel",
+email:
+"Courriel",
+password:
+"Mot de passe",
+confirm_password:
+"Confirmer le mot de passe",
+ok:
+"OK",
+send:
+"Envoyer",
+address_1:
+"Adresse",
+address_2:
+"Adresse 2",
+city:
+"Ville",
+postal_code:
+"Code postal",
+phone:
+"Téléphone",
+previous:
+"Étape précédente",
+next:
+"Étape suivante",
+finalize:
+"Placer la commande",
+country:
+"Pays",
+subtotal:
+"Sous-total",
+rebate:
+"Rabais",
+apply_promo_code:
+"Appliquer un code promo",
+my_cart:
+"Mon panier",
+continue_shopping:
+"Continuer à magasiner",
+my_cart_content:
+"Contenu de mon panier",
+shipping_method:
+"Méthode de livraison",
+payment_method: 
+"Méthode de paiement",
+confirm_order:
+"Confirmation de l'achat",
+bill_me_later:
+"Payer plus tard",
+bill_me_later_explanation:
+"Une facture vous sera envoyée par courriel.",
+promo_code_applied_successfully:
+"Votre code promo a été appliqué avec succès.",
+promo_code_is_invalid:
+"Le code promo est invalide.",
+promo_code_code:
+"Codes promos",
+promo_code_rate_on_order:
+"de rabais sur la commande",
+promo_code_alternate_price:
+"prix spécial sur certains articles",
+total:
+"Total",
+province_state:
+"Province / État",
+billing_address:
+"Adresse de facturation",
+shipping_address:
+"Adresse de livraison",
+payment_informations:
+"Informations de paiement",
+payment_informations_bill_me_later:
+"Je paierai plus tard",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Janvier",
+months_february:
+"Février",
+months_march:
+"Mars",
+months_april:
+"Avril",
+months_may:
+"Mai",
+months_june:
+"Juin",
+months_july:
+"Juillet",
+months_august:
+"Août",
+months_september:
+"Septembre",
+months_october:
+"Octobre",
+months_november:
+"Novembre",
+months_december:
+"Décembre",
+cart_items_table_item:
+"Article",
+cart_items_table_description:
+"Description",
+cart_items_table_quantity:
+"Quantité",
+cart_items_table_unit_price:
+"Prix unitaire",
+cart_items_table_total_price:
+"Prix total",
+cart_empty_text:
+"Votre panier est actuellement vide. Sélectionnez des produits afin de procéder au paiement.",
+new_account_form_create_new_account:
+"Créer un nouveau compte",
+new_account_form_create_new_account_action:
+"Créer",
+login_form_having_an_account:
+"Vous avez un compte ?",
+login_form_login_action:
+"Connexion",
+login_form_forgot_password_action:
+"J'ai oublié mon mot de passe",
+forgot_password_forgot_your_password:
+"Oublié votre mot de passe ?",
+forgot_password_please_enter_email:
+"Veuillez saisir votre courriel, nous vous enverrons un courriel contenant un lien unique qui vous permettra de réinitilaiser votre mot de passe",
+forgot_password_success_email_sent:
+"Le courriel a été envoyé",
+forgot_password_email_sent_message:
+"Un courriel vous a été envoyé avec les instructions pour réinitialiser votre mot de passe. Veuillez aller consulter celui-ci.",
+login_checkout_as_guest:
+"Payer sans compte",
+login_checkout_as_guest_notice:
+"À la fin du processus de paiement, nous vous offririons la possiblité de vous créer un compte avec les informations que vous aurez saisi durant le processus de paiement.",
+shipping_address_same_as_billing:
+"Mon adresse de livraison est identique à mon adresse de facturation",
+shipping_method_method_name:
+"Méthode de livraison",
+shipping_method_shipping_price:
+"Prix de la livraison",
+shipping_method_failure_message:
+"Nous n'avons pas été en mesure de trouver une méthode de livraison. Veuillez vous assurer que votre adresse de livraison est correcte et essayez à nouveau.",
+shipping_method_failure_click_here_to_edit:
+"Cliquez ici pour modifier votre adresse de livraison",
+payment_method_card_holder:
+"Nom sur la carte",
+payment_method_card_type:
+"Type de la carte",
+payment_method_card_number:
+"Numéro de la carte",
+payment_method_card_cvc:
+"CVC",
+payment_method_card_exp_month:
+"Mois / Année d'expiration",
+payment_method_card_exp_year:
+"Année d'expiration",
+payment_method_cvc_infos:
+"Le CVC est le code de sécurité à 3 chiffres habituellement en arrière de votre carte à droite de la signature.",
+create_an_account:
+"Créer un compte",
+why_create_account:
+"Pour commander plus rapidement la prochaine fois, entrez un mot de passe pour créer un compte.",
+reset_password:
+"Réinitialiser mon mot de passe",
+reset_password_success:
+"Votre mot de passe a été réinitialisé",
+reset_password_changed:
+"Votre mot de passe a été mis à jour.",
+reset_password_click_here_to_login:
+"Cliquez ici pour vous connecter",
+thankyou_message:
+"Merci pour votre commande!",
+thankyou_submessage:
+"Vous recevrez un courriel de confirmation bientôt",
+account_created_successfully:
+"Comtpe créé avec succès",
+account_created_successfully_message:
+"Votre compte a été créé avec succès, merci beaucoup.",
+errors_required:
+"Ce champ est requis",
+errors_email_must_be_unique:
+"Un usager avec le même courriel existe déjà",
+errors_both_password_must_match:
+"Les deux mots de passe doivent être identique",
+errors_email_must_be_valid:
+"Le courriel doit être valide",
+errors_email_does_not_match_any_existing_user:
+"Aucun usager n'existe avec ce courriel.",
+errors_email_does_not_match_reset_password_request:
+"La demande de réinitialisation de mot de passe ne concorde pas avec le courriel.",
+errors_reset_password_token_expired:
+"La demande de réinitialisation de mot de passe est expirée.",
+errors_invalid_authentication_infos:
+"Les informations de connexion sont invalides.",
+error_payment_items_empty:
+"Il semble que votre commande est invalide, veuillez rafraîchir la page. Votre carte de crédit n'a pas été chargée.",
+error_payment_items_are_invalid:
+"Nous n'avons pas pu complété votre commande. Il semble qu'un article dans votre panier a un prix invalide.",
+error_crawling_failed:
+"Nous n'avons pas pu validé votre commande. Votre carte de crédit n'a pas été chargée. Veuillez réessayer dans quelques instants.",
+powered_by:
+"Propulsé par",
+promocode_rate_format:
+"{0}% de rabais sur votre commande",
+promocode_amount_format:
+"{0} de rabais sur votre commande",
+shipping_method_business_days:
+"{0} jours ouvrables",
+shipping_method_business_day:
+"{0} jour ouvrable",
+welcome:
+"Bienvenue",
+back:
+"Retour",
+order_infos:
+"Infos sur la commande",
+generic_error_title:
+"Oops, une erreur est survenue.",
+promocode_deleted_at_checkout:
+"Le code promo que vous avez utilisé a atteint son nombre d'utilisation maximum durant votre paiement. Nous sommes désolés de cet inconvénient.",
+continue_shopping:
+"Continuer à magasiner",
+payment_required_message:
+"Le panier d'achat de ce site a été désactivé. Si vous êtes le propriétaire du site, veuillez vous connecter à votre compte Snipcart afin de résoudre le problème.",
+payment_require_title:
+"Le panier d'achat est désactivé."
+};
