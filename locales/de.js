866a5f4c6539696b22d59fc78629c569fac2f4ce
Memi Beltrame <memi@lessamess.ch>
2013-09-14 00:40:03 +0200

    Basic translations
    A few items still missing, due to ambiguous terms.

---
+++ locales/de.js
@@ -0,0 +1,248 @@
+﻿Snipcart.do('registerLocale', 'de', {
+yes:
+"Ja",
+no:
+"Nein",
+print:
+"Drucken",
+download_as_pdf:
+"Als PDF herunterladen",
+checkout:
+"Kasse",
+close:
+"Schliessen",
+name:
+"Name",
+company_name:
+"Firma",
+share_by_email:
+"Via Email teilen",
+email:
+"Email",
+password:
+"Passwort",
+confirm_password:
+"Passwort bestätigen",
+ok:
+"OK",
+send:
+"Senden",
+address_1:
+"Strasse",
+address_2:
+"Strasse 2",
+city:
+"Ort",
+postal_code:
+"PLZ",
+phone:
+"Telefon",
+previous:
+"Zurück",
+next:
+"Weiter",
+finalize:
+"Kostenpflichtig bestellen",
+country:
+"Land",
+subtotal:
+"Zwischensumme",
+rebate:
+"Rabatt",
+apply_promo_code:
+"Promotionscode einlösen",
+my_cart:
+"Mein Warenkorb",
+continue_shopping:
+"Einkauf forrtsetzen",
+my_cart_content:
+"In meinem Warenkorb",
+shipping_method:
+"Versandart",
+payment_method:
+"Zahlungsart",
+confirm_order:
+"Kauf bestätigen",
+bill_me_later:
+"Später bezahlen",
+bill_me_later_explanation:
+"Eine Rechnung wird Ihnen via Email zugestellt.",
+promo_code_applied_successfully:
+"Ihr Promotionscode wurde erfolgreich eingelöst.",
+promo_code_is_invalid:
+"Ihr Promotionscode ist ungültig.",
+promo_code_code:
+"Haben Sie einen Promotionscode?",
+promo_code_rate_on_order:
+"Rabatt auf die Bestellung",
+promo_code_alternate_price:
+"Sonderpreis auf gewissen Produkten",
+total:
+"Total",
+province_state:
+"BundesLand / Kanton",
+billing_address:
+"Rechnungsadresse",
+shipping_address:
+"Versandadresse",
+payment_informations:
+"Zahlungsinformation",
+payment_informations_bill_me_later:
+"Ich bezahle später",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Januar",
+months_february:
+"Februar",
+months_march:
+"März",
+months_april:
+"April",
+months_may:
+"Mai",
+months_june:
+"Juni",
+months_july:
+"Juli",
+months_august:
+"August",
+months_september:
+"September",
+months_october:
+"Oktober",
+months_november:
+"November",
+months_december:
+"Dezember",
+cart_items_table_item:
+"Artikel",
+cart_items_table_description:
+"Beschreibung",
+cart_items_table_quantity:
+"Menge",
+cart_items_table_unit_price:
+"Stückpreis",
+cart_items_table_total_price:
+"Gesamt Preis",
+cart_empty_text:
+"Der Warenkorb ist leer. Wählen Sie Produkte aus, bevor Sie zur Kasse gehen.",
+new_account_form_create_new_account:
+"Ein Konto erstellen",
+new_account_form_create_new_account_action:
+"erstellen",
+login_form_having_an_account:
+"Haben Sie ein Konto?",
+login_form_login_action:
+"Einloggen",
+login_form_forgot_password_action:
+"Ich habe mein Passwort vergessen.",
+forgot_password_forgot_your_password:
+"Passwort vergessen?",
+forgot_password_please_enter_email:
+"Geben Sie bite Ihre Emailadresse ein, wir werden Ihnen eine Email mit einem einmaligen Link senden, mit dem Sie ihr Passwort zurücksetzen können.",
+forgot_password_success_email_sent:
+"Email versendet",
+forgot_password_email_sent_message:
+"Eine Email mit einer Anleitung zum Zurücksetzen Ihres Passworts wurde an Sie gesendet. Öffen Sie dieses und folgen Sie den angegebenen Schritten.",
+login_checkout_as_guest:
+"Als Gast bezahlen",
+login_checkout_as_guest_notice:
+"Nach dem Abschliessen der Bestellung haben Sie die Möglichkeit mit den gemachten Angaben einfach ein Konto zu eröffnen.",
+shipping_address_same_as_billing:
+"Versandadresse ist mit Rechnungsadresse identisch",
+shipping_method_method_name:
+"Versandart",
+shipping_method_shipping_price:
+"Versandpreis",
+shipping_method_failure_message:
+"Wir konnten keine mögliche Versandart finden. Bitte stellen Sie sicher, dass Ihre Adresse korrekt ist und versuchen Sie es nochmals.",
+shipping_method_failure_click_here_to_edit:
+"Versandadresse bearbeiten",
+payment_method_card_holder:
+"Name auf Karte",
+payment_method_card_type:
+"Kartentyp",
+payment_method_card_number:
+"Kartennummer",
+payment_method_card_cvc:
+"CVC",
+payment_method_card_exp_month:
+"Ablauf Monat / Jahr",
+payment_method_card_exp_year:
+"Ablauf Jahr",
+payment_method_cvc_infos:
+"Die CVC die 3-stellige Sicherheits-Zahl auf der Rückseite Ihrer Karte, gewöhnlicherweise recht von Ihrer Unterschrift.",
+create_an_account:
+"Konto erstellen",
+why_create_account:
+"Erstellen Sie ein Konto damit Sie bei Ihrem nächsten Kauf Zeit sparen. Geben Sie ein Passwort an, um ein Konto zu erstellen.",
+reset_password:
+"Passwort zurücksetzen",
+reset_password_success:
+"Das Passwort wurde zurückgesetzt",
+reset_password_changed:
+"Ihr Passwort wurde geändert.",
+reset_password_click_here_to_login:
+"Hier klicken zum einloggen",
+thankyou_message:
+"Danke für Ihre Bestellung!",
+thankyou_submessage:
+"Wir haben Ihnen eine Bestätigung geschickt.",
+account_created_successfully:
+"Konto erfolgreich erstellt!",
+account_created_successfully_message:
+"Ihr Konto wurde erfolgreich erstellt, herzlichen Dank.",
+errors_required:
+"Das ist ein Pflichtfeld",
+errors_email_must_be_unique:
+"Ein Benutzer mit der gleichen Emailadresse ist bereits vorhanden.",
+errors_both_password_must_match:
+"Die Beiden Passwörter müssen identisch sein",
+errors_email_must_be_valid:
+"Die Emailadresse muss gültig sein.",
+errors_email_does_not_match_any_existing_user:
+"Es exisitiert kein Benutzer mit dieser Emailadresse.",
+errors_email_does_not_match_reset_password_request:
+"User email does not match any existing reset password request.",
+errors_reset_password_token_expired:
+"Die Anfrage zur Passwort-Zurücksetzung ist veraltet.",
+errors_invalid_authentication_infos:
+"Ungültige Identifizierungsangaben",
+error_payment_items_empty:
+"Ihre Bestellung scheint ungültig zu sein. Bitte laden Sie die Seite neu. Ihre Kreditkarte wurde nicht belastet.",
+error_payment_items_are_invalid:
+"Wir konnten Ihre Bestellung nicht abschliessen. Eines der Produkte in Ihrem Warenkorb schein einen ungültigen Preis zu haben.",
+error_crawling_failed:
+"Wir konnten Ihre Bestellung nicht abschliessen. Ihre Kreditkarte wurde nicht belastet. Bitte versuchen Sie es in wenigen Augenblicken nochmals.",
+powered_by:
+"Unterstützt durch",
+promocode_rate_format:
+"{0}% Rabatt auf Ihre Bestellung",
+promocode_amount_format:
+"{0} Rabatt auf Ihre Bestellung",
+shipping_method_business_days:
+"{0} Arbeitstage",
+shipping_method_business_day:
+"{0} Arbeitstage",
+welcome:
+"Willkommen",
+back:
+"Zurück",
+order_infos:
+"Bestellangaben",
+generic_error_title:
+"Hoppla, da ist ein Fehler passiert!",
+promocode_deleted_at_checkout:
+"The promocode you used has reached its usage limit while you were doing your checkout. Sorry for this inconvenience.",
+continue_shopping:
+"Einkauf fortsetzen",
+payment_required_message:
+"The shopping cart for this website has been disabled. If you are the owner of this website, please login into Snipcart dashboard to resolve the issue.",
+payment_require_title:
+"Der Warenkorb ist deaktiviert."
+});
