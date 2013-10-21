c0f47f69595025947b73155791629d0dd275ecff
Charles Ouellet <charles.ouellet@gmail.com>
2013-10-21 08:41:15 -0400

    Added Swedish translation

---
+++ locales/sv.js
@@ -0,0 +1,250 @@
+Snipcart.do('registerLocale', 'sv', {
+yes:
+"Ja",
+no:
+"Nej",
+print:
+"Skriv ut",
+download_as_pdf:
+"Ladda ner som PDF",
+checkout:
+"Till kassan",
+close:
+"Stäng",
+name:
+"Namn",
+company_name:
+"Företagsnamn",
+share_by_email:
+"Dela via E-mail",
+email:
+"Email",
+password:
+"Lösenord",
+confirm_password:
+"Bekräfta lösenord",
+ok:
+"Ok",
+send:
+"Skicka",
+address_1:
+"Gatuadress",
+address_2:
+"Gatuadress 2",
+city:
+"Stad",
+postal_code:
+"Postkod",
+phone:
+"Telefon",
+previous:
+"Föregående steg",
+next:
+"Nästa steg",
+finalize:
+"Skicka din order",
+country:
+"Land",
+subtotal:
+"Delsumma",
+rebate:
+"Rabatt",
+apply_promo_code:
+"Använd kampanjkod",
+my_cart:
+"Min kundvagn",
+continue_shopping:
+"Fortsätt shoppa",
+my_cart_content:
+"Min kundvagn innehåller",
+shipping_method:
+"Fraktalternativ",
+payment_method:
+"Betalningsalternativ",
+confirm_order:
+"Bekräfta order",
+bill_me_later:
+"Betalning mot faktura",
+bill_me_later_explanation:
+"En faktura kommer att skickas till dig via E-mail",
+promo_code_applied_successfully:
+"Din kampanjkod har blivit tillagd",
+promo_code_is_invalid:
+"Kmpanjkoden är ogiltig",
+promo_code_code:
+"Har du en kampanjkod?",
+promo_code_rate_on_order:
+"Rabatt på din order",
+promo_code_alternate_price:
+"Specialpris på produkter",
+total:
+"Totalt",
+province_state:
+"Län",
+billing_address:
+"Faktureringsadress",
+shipping_address:
+"Leveransadress",
+payment_informations:
+"Betalningsinformation",
+payment_informations_bill_me_later:
+"Jag vill betala mot faktura",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Januari",
+months_february:
+"Februari",
+months_march:
+"Mars",
+months_april:
+"April",
+months_may:
+"Maj",
+months_june:
+"Juni",
+months_july:
+"July",
+months_august:
+"Augusti",
+months_september:
+"September",
+months_october:
+"Oktober",
+months_november:
+"November",
+months_december:
+"December",
+cart_items_table_item:
+"Artikel",
+cart_items_table_description:
+"Beskrivning",
+cart_items_table_quantity:
+"Antal",
+cart_items_table_unit_price:
+"Pris",
+cart_items_table_total_price:
+"Totalpris",
+cart_empty_text:
+"Kundvagnen är tom, Välj någon produkt innan du går till kassan",
+new_account_form_create_new_account:
+"Skapa en inloggning",
+new_account_form_create_new_account_action:
+"Skapa en inloggning",
+login_form_having_an_account:
+"Logga in",
+login_form_login_action:
+"Logga in",
+login_form_forgot_password_action:
+"Jag har glömt mitt lösenord",
+forgot_password_forgot_your_password:
+"Glömt ditt lösenord?",
+forgot_password_please_enter_email:
+"Ange din e-postadress så skickar vi ett e-postmeddelande med en unik länk för att återställa ditt lösenord.",
+forgot_password_success_email_sent:
+"E-post skickat",
+forgot_password_email_sent_message:
+"Ett e-postmeddelande har skickats till dig med instruktioner om hur du återställer ditt lösenord. Vänligen kolla din e-post och följ anvisningarna.",
+login_checkout_as_guest:
+"Till kassan som en gäst",
+login_checkout_as_guest_notice:
+"Vid slutet av kassan, kommer du att erbjudas möjligheten att skapa ett konto med hjälp av den information du angett tidigare.",
+shipping_address_same_as_billing:
+"Samma som fakturaadress",
+shipping_method_method_name:
+"Fraktalternativ",
+shipping_method_shipping_price:
+"Frakt pris",
+shipping_method_failure_message:
+"Vi har tyvärr inte kunnat hitta något möjligt fraktalternativ. Vänligen kolla så att din leveransadress är korrekt och försök igen.",
+shipping_method_failure_click_here_to_edit:
+"Klicka här för att ändra din leveransadress",
+payment_method_card_holder:
+"Namn på kortet",
+payment_method_card_type:
+"Korttyp",
+payment_method_card_number:
+"Kortnummer",
+payment_method_card_cvc:
+"CVC",
+payment_method_card_exp_month:
+"Giltigt till månad / år",
+payment_method_card_exp_year:
+"Giltigt till år",
+payment_method_cvc_infos:
+"CVC är ett säkerhetsnummer innehållande 3 siffror och finns bakom ditt kreditkort, oftast till höger om din signatur.",
+create_an_account:
+"Skapa en inloggning",
+why_create_account:
+"För en snabbare process vid din nästa beställning, ange bara ett lösenord för att skapa din inloggning.",
+reset_password:
+"Återställ lösenord",
+reset_password_success:
+"Lösenordet är nu återställt.",
+reset_password_changed:
+"Ditt lösenord har blivit ändrat.",
+reset_password_click_here_to_login:
+"Klicka här för att logga in",
+thankyou_message:
+"Tack för din beställning!",
+thankyou_submessage:
+"Du kommer snart att få ett bekräftelsemail.",
+account_created_successfully:
+"Ditt konto har nu skapats",
+account_created_successfully_message:
+"Ditt konto har nu skapats, Tack så mycket!",
+errors_required:
+"Detta är ett obligatoriskt fält",
+errors_email_must_be_unique:
+"En användare med samma e-postadress finns redan",
+errors_both_password_must_match:
+"Båda lösenorden måste vara lika",
+errors_email_must_be_valid:
+"E-postadressen måste vara giltig",
+errors_email_does_not_match_any_existing_user:
+"Det finns ingen användare med denna e-postadress",
+errors_email_does_not_match_reset_password_request:
+"För den angivna e-postadressen existerar ingen begäran om återställning av lösenordet.",
+errors_reset_password_token_expired:
+"Begäran om återställning av lösenordet hat upphört.",
+errors_invalid_authentication_infos:
+"Ogiltiga identifieringsuppgifter",
+error_payment_items_empty:
+"Det verkar som att din beställning är ogiltigt, vänligen ladda om sidan. Ditt kreditkort har inte debiterats.",
+error_payment_items_are_invalid:
+"Vi har inte kunnat slutföra din beställning. Det verkar som att en av de artiklar i din varukorg har en ogiltig pris.",
+error_crawling_failed:
+"Vi har inte kunnat bekräfta din beställning, ditt kreditkort inte har debiterats, vänligen försök igen om en stund.",
+powered_by:
+"Powered by",
+promocode_rate_format:
+"{0}% rabatt på din beställning",
+promocode_amount_format:
+"{0} rabatt på din beställning",
+shipping_method_business_days:
+"{0} arbetsdagar",
+shipping_method_business_day:
+"{0} arbetsdagar",
+shipping_method_delivery_time:
+"Till {0}", // By 2013-11-28
+welcome:
+"Välkommen",
+back:
+"Tillbakak",
+order_infos:
+"Beställningsinformation",
+generic_error_title:
+"Oops, ett fel inträffade.",
+promocode_deleted_at_checkout:
+"Den kampanjkod du använder har tyvärr nått det maximala antalet medan du var kvar i kassan. Vi ber om ursäkt för besväret.",
+continue_shopping:
+"Fortsätt shoppa",
+payment_required_message:
+"Kundvagnen för denna webbplats har inaktiverats. Om du är ägare av denna webbplats, vänligen logga in Snipcart för att lösa problemet.",
+payment_require_title:
+"Kundvagnen är inaktiverad"
+});
