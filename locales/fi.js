c0eb87e77fa8e34f2c42c9445b4ddb5e46eac49f
Charles Ouellet <charles.ouellet@gmail.com>
2015-12-14 15:46:09 -0500

    Added Finnish translation

---
+++ locales/fi.js
@@ -0,0 +1,420 @@
+Snipcart.execute('registerLocale', 'fi', {
+yes:
+"Kyllä",
+no:
+"Ei",
+print:
+"Tulosta",
+download_as_pdf:
+"Lataa PDF",
+checkout:
+"Vahvista",
+close:
+"Sulje",
+name:
+"Nimi",
+company_name:
+"Yrityksen nimi",
+share_by_email:
+"Lähetä sähköposti",
+email:
+"Sähköposti",
+password:
+"Salasana",
+confirm_password:
+"Vahvista salasana",
+ok:
+"OK",
+send:
+"Lähetä",
+address_1:
+"Katuosoite",
+address_2:
+"Postiosoite",
+city:
+"Paikkakunta",
+postal_code:
+"Postinumero",
+phone:
+"Puhelin",
+previous:
+"Edellinen",
+next:
+"Seuraava",
+finalize:
+"Tilaa",
+country:
+"Maa",
+subtotal:
+"Välisumma",
+rebate:
+"Alennus",
+apply_promo_code:
+"Alennuskoodi",
+my_cart:
+"Tilaus",
+my_cart_content:
+"Tilauksen sisältö",
+shipping_method:
+"Toimitustapa",
+payment_method:
+"Maksutapa",
+confirm_order:
+"Vahvista tilaus",
+bill_me_later:
+"Lasku",
+bill_me_later_explanation:
+"Lasku lähetetään sähköpostiin",
+promo_code_applied_successfully:
+"Alennus on laskettu",
+promo_code_is_invalid:
+"Alennuskoodi on väärä",
+promo_code_code:
+"Alennus",
+promo_code_rate_on_order:
+"alennus tilauksesta",
+promo_code_alternate_price:
+"Erikoishinnat",
+total:
+"Yhteensä",
+total_paid:
+"Loppusumma",
+province_state:
+"Paikkakunta",
+billing_address:
+"Laskutusosoite",
+shipping_address:
+"Toimitusosoite",
+payment_informations:
+"Maksutiedot",
+payment_informations_bill_me_later:
+"Maksan myöhemmin",
+payment_informations_paypalexpress:
+"Paypal-maksu",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Tammikuu",
+months_february:
+"Helmikuu",
+months_march:
+"Maaliskuu",
+months_april:
+"Huhtikuu",
+months_may:
+"Toukokuu",
+months_june:
+"Kesäkuu",
+months_july:
+"Heinäkuu",
+months_august:
+"Elokuu",
+months_september:
+"Syyskuu",
+months_october:
+"Lokakuu",
+months_november:
+"Marraskuu",
+months_december:
+"Joulukuu",
+cart_items_table_item:
+"Tuote",
+cart_items_table_description:
+"Kuvaus",
+cart_items_table_quantity:
+"Määrä",
+cart_items_table_unit_price:
+"Hinta",
+cart_items_table_total_price:
+"Summa",
+cart_empty_text:
+"Tilauksessa ei ole tuotteita. Valitse tuote.",
+new_account_form_create_new_account:
+"Luo käyttäjätunnus",
+new_account_form_create_new_account_action:
+"Luo käyttäjätunnus",
+login_form_having_an_account:
+"Kirjautuminen",
+login_form_login_action:
+"Kirjaudu",
+login_title:
+"Kirjautuminen",
+login_form_forgot_password_action:
+"Unohdin salasanani",
+forgot_password_forgot_your_password:
+"Unohtuiko salasana?",
+forgot_password_please_enter_email:
+"Anna sähköpostiosoite, johon lähetämme linkin salasanan vaihtoa varten.",
+forgot_password_success_email_sent:
+"Sähköposti lähetetty",
+forgot_password_email_sent_message:
+"Sähköpostissa on viesti, miten vaihdat salasanasi. Avaa sähköposti ja seuraa ohjetta.",
+login_checkout_as_guest:
+"Jatka rekisteröitymättä",
+login_checkout_as_guest_notice:
+"Tilausprosessin lopussa voit luoda käyttäjätilin antamillasi tiedoilla.",
+shipping_address_same_as_billing:
+"Käytä tätä toimitusosoitetta",
+shipping_method_method_name:
+"Toimitustapa",
+shipping_method_shipping_price:
+"Toimituksen hinta",
+shipping_method_failure_message:
+"Emme ole löytäneet toimitustapaa. Tarkista, että toimitusosoite on oikea ja yritä uudelleen.",
+shipping_method_failure_click_here_to_edit:
+"Klikkaa tähän ja päivitä toimitusosoite",
+payment_method_card_holder:
+"Kortin omistaja",
+payment_method_card_type:
+"Korttityyppi",
+payment_method_card_number:
+"Kortin numero",
+payment_method_card_cvc:
+"CSC",
+payment_method_card_exp_month:
+"Voimassaolokuukausi",
+payment_method_card_exp_year:
+"Voimassaolovuosi",
+payment_method_cvc_infos:
+"CVC on kolminumeroinen turvallisuusnumero luottokortin takana, yleensä allekirjoituksen oikealla puolella.",
+create_an_account:
+"Luo käyttäjätunnus",
+why_create_account:
+"Seuraavan tilauksen nopeuttamiseksi, anna salasana käyttäjätunnuksen luontia varten.",
+reset_password:
+"Vaihda salasana",
+reset_password_success:
+"Salasana vaihdettu",
+reset_password_changed:
+"Salasanasi on päivitetty.",
+reset_password_click_here_to_login:
+"Kirjaudu tästä.",
+thankyou_message:
+"Kiitos tilauksesta! Lasku lähetetään sähköpostiin, saat sen pian.",
+thankyou_submessage:
+"Saat pian vahvistuksen sähköpostiin.",
+account_created_successfully:
+"Tili on luotu",
+account_created_successfully_message:
+"Tilin luonti on onnistunut, kiitos.",
+errors_required:
+"Pakollinen kenttä",
+errors_email_must_be_unique:
+"Käyttäjä on jo olemassa",
+errors_both_password_must_match:
+"Molempien salasanojen pitää olla samat",
+errors_email_must_be_valid:
+"Syötä oikea sähköpostiosoite",
+errors_email_does_not_match_any_existing_user:
+"Käyttäjätunnusta ei löydy",
+errors_email_does_not_match_reset_password_request:
+"Sähköposti ja salasana eivät täsmää.",
+errors_reset_password_token_expired:
+"Salasanan vaihtolinkki on vanhentunut.",
+errors_invalid_authentication_infos:
+"Tunnistautuminen epäonnistui",
+error_payment_items_empty:
+"Tilaus epäonnistui, lataa sivu uudelleen. Luottokorttia ei ole veloitettu.",
+error_payment_items_are_invalid:
+"Tilauksen käsittely ei ole onnistunut. Yhden tuotteen hinnassa on virhe.",
+error_crawling_failed:
+"Tilaus ei ole onnistunut, luottokorttia ei ole veloitettu. Ole hyvä ja yritä uudelleen vähän ajan kuluttua.",
+powered_by:
+"Powered and secured by",
+promocode_rate_format:
+"{0}% alennus tilauksesta",
+promocode_amount_format:
+"{0} alennus tilauksesta",
+shipping_method_business_days:
+"{0} arkipäivää",
+shipping_method_business_day:
+"{0} arkipäivää",
+shipping_method_delivery_time:
+"Mennessä {0}", // By 2013-11-28
+welcome:
+"Tervetuloa",
+back:
+"Takaisin",
+order_infos:
+"Tilauksien tiedot",
+generic_error_title:
+"Oops, tapahtui virhe.",
+promocode_deleted_at_checkout:
+"Alennus ei ole enää voimassa. Pahoittelemme.",
+continue_shopping:
+"Jatka tilaamista",
+payment_required_message:
+"Kauppa ei toimi. Jos olet sivuston omistaja, kirjaudu Snipcartin dashboardiin asian ratkaisemiseksi.",
+payment_require_title:
+"Kauppa ei toimi.",
+configuration_problem:
+"Konfiguraatio-ongelma",
+additionnal_information:
+"Kirjoita viesti, jos halaut lähettää palautetta tai lisätietoa tästä ongelmasta.",
+send_error:
+"Lähetä virheilmoitus sivuston omistajalle",
+message_sent:
+"Viesti lähetetty, kiitos",
+paypalexpress_loading:
+"Sinut ohjataan maksamaan Paypalin sivustolle.",
+paypalexpress_cancelled:
+"Peruutit tapahtuman. Voit klikata allaolevaa painiketta tai siirtyä kauppaan.",
+retry:
+"Yritä uudelleen",
+error_crawlingfailed_title:
+"Tilaaminen ei onnistunut, ei veloitusta.",
+error_crawling_unreachable:
+"Tuotetta<strong>{0}</strong> ei löydy <strong>{1}</strong>. Varmista, että tuotelinkki toimii.",
+error_crawling_product_not_found:
+"Tuotetta <strong>{0}</strong> ei löydy <strong>{1}</strong>.",
+error_crawling_price_not_found:
+"Tuotteelle <strong>{0}</strong> ei löydy hintaa<strong>{1}</strong>, määritä tuotteelle hinta.",
+error_crawling_price_doesnot_match:
+"Tuotteen <strong>{0}</strong> hinta<strong>{3}</strong> on <strong>{2}</strong> mutta sen pitäisi olla <strong>{1}</strong>.",
+error_crawlingfailed_title_test:
+"Tuotteen tilaaminen ei onnistunut, ei veloitusta. Sivusto on tällä hetkellä testitilassa.",
+order_completedon:
+"Tilaus on valmis",
+payment_method_status:
+"Maksusuorituksen status",
+payment_method_status_approved:
+"Hyväksytty",
+order_reference_number:
+"Viitenumero",
+order_transaction_amount:
+"Suorituksen summa",
+order_invoice_number:
+"Laskun numero",
+order_authorization_code:
+"Auktorisointikoodi",
+item_is_being_added:
+"Tuote on lisätty tilaukseen...",
+order_completing_payment:
+"Tilaan...",
+calculating_shipping_fees:
+"Lasken toimitusmaksun...",
+saving:
+"Tallennan...",
+loading:
+"Lataan...",
+free_shipping:
+"Ilmainen toimitus",
+cart_plans_name:
+"Plan",
+cart_plans_interval:
+"Interval",
+cart_plans_quantity:
+"Määrä",
+cart_plans_amount:
+"Summat",
+cart_plans_total:
+"Yhteensä",
+payable_now:
+"Maksettava",
+upcoming_payment_for:
+"Tilaus",
+upcoming_payment_date:
+"Päivämäärä",
+upcoming_payment_amount:
+"Yhteensä",
+upcoming_payment_subtotal:
+"Välisumma",
+upcoming_payments:
+"Seuraavat maksut",
+order_date:
+"Päiväys",
+order_status:
+"Status",
+order_total:
+"Yhteensä",
+orders_history:
+"Tilaushistoria",
+orders_history_no_orders:
+"Ei vielä tilauksia.",
+orders_fetching_orders:
+"Lataan tilaukset...",
+user_nav_orders:
+"Tilaukset",
+user_nav_subscriptions:
+"Tilaukset",
+user_nav_cart:
+"Tilaukset",
+subscriptions_history:
+"Tilaushistoria",
+subscription_name:
+"Tuote",
+subscription_amount:
+"Summa",
+subscription_quantity:
+"Määrä",
+subscription_total_amount:
+"Yhteensä",
+subscriptions_fetching_subscriptions:
+"Lataan tilaukset...",
+subscriptions_fetching_details:
+"Lataan tilauksen tiedot...",
+subscription_no_invoices:
+"Tilausta ei ole vielä laskutettu.",
+subscription_plan_name:
+"Plan name",
+subscription_plan_interval:
+"Plan interval",
+subscription_invoice_number:
+"Numero",
+subscription_invoice_date:
+"Päivämäärä",
+subscription_invoice_amount:
+"Summa",
+subscription_invoice_item:
+"Tuote",
+subscription_invoice_info:
+"Lasku",
+subscription_invoice_details:
+"Laskutiedot",
+subscription_invoice_fetching_details:
+"Lataan laskutiedot...",
+subscription_invoices_loading:
+"Lataan laskut...",
+subscription_invoices_list:
+"Laskutushistoria",
+item_out_of_stock:
+"Pahoittelemme mutta tuotetta ei ole varastossa.",
+click_here_to_remove_it:
+"Klikkaa tähän ja poista tuote tilauksesta.",
+outofstock_notification:
+"Pahoittelemme mutta jotkut tilauksessa olevista tuotteista loppuivat varastosta. Tarkista tilaus. Korttia ei ole veloitettu.",
+plan_amount_per_day:
+"{0} / päivä",
+plan_amount_per_week:
+"{0} / viikko",
+plan_amount_per_month:
+"{0} / kuukausi",
+plan_amount_per_year:
+"{0} / vuosi",
+plan_days_of_trial:
+"{0} päivän testiaika",
+subscription_cancel_button:
+"Peruuta tilaus",
+subscription_cancel_confirmation:
+"Oletko varma, että halaut peruuttaa tilauksesi?",
+subscription_cancel_success:
+"Tilauksesi on peruutettu.",
+subscription_cancelled_notice:
+"Tilaus ei ole enää voimassa, se on peruutettu {0}.",
+subscription_cancelledon:
+"Peruutettu",
+paypal_express_checkout_link:
+"Maksa Paypalilla",
+paypal_express_checkout_title:
+"Maksa PayPalilla",
+paypal_express_checkout_explanation:
+"Klikkaa tätä nappulaa, mikäli haluat maksaa PayPalilla.",
+notifications_item_not_added_due_to_max_quantity:
+"Tuotetta ei voi lisätä, tilauksessa on jo enimmäismäärä tuotteita.",
+error_impossible_to_calculate_taxes:
+"Veroja ei voida laskea, mikä voi johtua väärästä osoitteesta. Varmista, että antamasi postinumero on oikea."
+});
