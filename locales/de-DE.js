c3e0c68da8233a7652915ec899a2e6635c8909dc
Charles Ouellet <charles.ouellet@gmail.com>
2017-03-22 09:52:13 -0400

    Fixed locale

---
+++ locales/de-DE.js
@@ -0,0 +1,422 @@
+Snipcart.execute('registerLocale', 'de-DE', {
+yes:
+"Ja",
+no:
+"Nein",
+print:
+"Drucken",
+download_as_pdf:
+"Als PDF herunterladen",
+checkout:
+"Zur Kasse",
+close:
+"Schließen",
+name:
+"Name",
+company_name:
+"Firma",
+share_by_email:
+"Via E-Mail teilen",
+email:
+"E-Mail",
+password:
+"Passwort",
+confirm_password:
+"Passwort bestätigen",
+ok:
+"OK",
+send:
+"Senden",
+address_1:
+"Straße",
+address_2:
+"Straße 2",
+city:
+"Ort",
+postal_code:
+"PLZ",
+phone:
+"Telefon",
+previous:
+"Zurück",
+next:
+"Weiter",
+finalize:
+"Kostenpflichtig bestellen",
+country:
+"Land",
+subtotal:
+"Zwischensumme",
+rebate:
+"Rabatt",
+apply_promo_code:
+"Gutschein einlösen",
+my_cart:
+"Mein Warenkorb",
+my_cart_content:
+"In meinem Warenkorb",
+shipping_method:
+"Versandart",
+payment_method:
+"Zahlungsart",
+confirm_order:
+"Kauf bestätigen",
+bill_me_later:
+"Später bezahlen",
+bill_me_later_explanation:
+"Eine Rechnung wird Ihnen via E-Mail zugestellt.",
+promo_code_applied_successfully:
+"Ihr Gutschein wurde erfolgreich eingelöst.",
+promo_code_is_invalid:
+"Dieser Gutscheincode ist ungültig.",
+promo_code_is_expired:
+"Dieser Gutscheincode ist abgelaufen.",
+promo_code_code:
+"Haben Sie einen Gutschein?",
+promo_code_rate_on_order:
+"Rabatt auf Ihre Bestellung",
+promo_code_alternate_price:
+"Sonderpreis auf gewissen Produkten",
+total:
+"Summe",
+total_paid:
+"Summe bezahlt",
+province_state:
+"Bundesland / Kanton",
+billing_address:
+"Rechnungsadresse",
+shipping_address:
+"Lieferadresse",
+payment_informations:
+"Zahlung",
+payment_informations_bill_me_later:
+"Ich bezahle später",
+payment_informations_paypalexpress:
+"Zahlung mit Paypal",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Januar",
+months_february:
+"Februar",
+months_march:
+"März",
+months_april:
+"April",
+months_may:
+"Mai",
+months_june:
+"Juni",
+months_july:
+"Juli",
+months_august:
+"August",
+months_september:
+"September",
+months_october:
+"Oktober",
+months_november:
+"November",
+months_december:
+"Dezember",
+cart_items_table_item:
+"Artikel",
+cart_items_table_description:
+"Beschreibung",
+cart_items_table_quantity:
+"Menge",
+cart_items_table_unit_price:
+"Stückpreis",
+cart_items_table_total_price:
+"Gesamtpreis",
+cart_empty_text:
+"Der Warenkorb ist leer. Wählen Sie Produkte aus, bevor Sie zur Kasse gehen.",
+new_account_form_create_new_account:
+"Ein Konto erstellen",
+new_account_form_create_new_account_action:
+"Konto Erstellen",
+login_form_having_an_account:
+"Haben Sie ein Konto?",
+login_form_login_action:
+"Einloggen",
+login_title:
+"Einloggen",
+login_form_forgot_password_action:
+"Ich habe mein Passwort vergessen.",
+forgot_password_forgot_your_password:
+"Passwort vergessen?",
+forgot_password_please_enter_email:
+"Geben Sie bitte Ihre E-Mail-Adresse ein, wir werden Ihnen eine E-Mail mit einem einmaligen Link senden, mit dem Sie Ihr Passwort zurücksetzen können.",
+forgot_password_success_email_sent:
+"E-Mail versendet",
+forgot_password_email_sent_message:
+"Eine E-Mail mit einer Anleitung zum Zurücksetzen Ihres Passworts wurde an Sie gesendet. Öffen Sie diese, und folgen Sie den angegebenen Schritten.",
+login_checkout_as_guest:
+"Als Gast bezahlen",
+login_checkout_as_guest_notice:
+"Nach dem Abschließen der Bestellung haben Sie die Möglichkeit, mit den gemachten Angaben einfach ein Konto zu erstellen.",
+shipping_address_same_as_billing:
+"Diese Adresse als Lieferadresse verwenden",
+shipping_method_method_name:
+"Versandart",
+shipping_method_shipping_price:
+"Versandkosten",
+shipping_method_failure_message:
+"Wir konnten keine passende Versandart finden. Bitte stellen Sie sicher, dass Ihre Adresse korrekt ist und versuchen Sie es nochmals.",
+shipping_method_failure_click_here_to_edit:
+"Lieferadresse bearbeiten",
+payment_method_card_holder:
+"Name auf Karte",
+payment_method_card_type:
+"Kartentyp",
+payment_method_card_number:
+"Kartennummer",
+payment_method_card_cvc:
+"CVC",
+payment_method_card_exp_month:
+"Ablauf Monat",
+payment_method_card_exp_year:
+"Ablauf Jahr",
+payment_method_cvc_infos:
+"Die CVC die 3-stellige Sicherheits-Zahl auf der Rückseite Ihrer Karte, normalerweise rechts von Ihrer Unterschrift.",
+create_an_account:
+"Konto erstellen",
+why_create_account:
+"Erstellen Sie ein Konto, damit Sie bei Ihrem nächsten Einkauf Zeit sparen. Geben Sie ein Passwort an, um ein Konto zu erstellen.",
+reset_password:
+"Passwort zurücksetzen",
+reset_password_success:
+"Das Passwort wurde zurückgesetzt",
+reset_password_changed:
+"Ihr Passwort wurde geändert.",
+reset_password_click_here_to_login:
+"Hier klicken zum Einloggen",
+thankyou_message:
+"Vielen Dank für Ihre Bestellung!",
+thankyou_submessage:
+"Wir haben Ihnen eine Bestätigungs-E-Mail geschickt.",
+account_created_successfully:
+"Konto erfolgreich erstellt!",
+account_created_successfully_message:
+"Ihr Konto wurde erfolgreich erstellt, vielen Dank.",
+errors_required:
+"Dies ist ein Pflichtfeld",
+errors_email_must_be_unique:
+"Ein Benutzer mit der gleichen E-Mail-Adresse ist bereits vorhanden.",
+errors_both_password_must_match:
+"Die beiden Passwörter müssen identisch sein",
+errors_email_must_be_valid:
+"Die E-Mail-Adresse muss gültig sein.",
+errors_email_does_not_match_any_existing_user:
+"Es exisitiert kein Benutzer mit dieser E-Mail-Adresse.",
+errors_email_does_not_match_reset_password_request:
+"Für die angegebene E-Mail-Adresse existiert kein Antrag zur Zurücksetzung des Passworts.",
+errors_reset_password_token_expired:
+"Dieser Link zum Zurücksetzen des Passworts ist abgelaufen.",
+errors_invalid_authentication_infos:
+"Ungültige Identifizierungsangaben",
+error_payment_items_empty:
+"Ihre Bestellung scheint ungültig zu sein. Bitte laden Sie die Seite neu. Es wurde keine Zahlung ausgelöst.",
+error_payment_items_are_invalid:
+"Wir konnten Ihre Bestellung nicht abschließen. Eines der Produkte in Ihrem Warenkorb scheint einen ungültigen Preis zu haben.",
+error_crawling_failed:
+"Wir konnten Ihre Bestellung nicht abschließen. Ihre Kreditkarte wurde nicht belastet. Bitte versuchen Sie es in wenigen Augenblicken nochmal.",
+powered_by:
+"Powered by",
+promocode_rate_format:
+"{0}% Rabatt auf Ihre Bestellung",
+promocode_amount_format:
+"{0} Rabatt auf Ihre Bestellung",
+shipping_method_business_days:
+"{0} Arbeitstage",
+shipping_method_business_day:
+"{0} Arbeitstag",
+shipping_method_delivery_time:
+"Bis {0}",
+welcome:
+"Willkommen",
+back:
+"Zurück",
+order_infos:
+"Bestellangaben",
+generic_error_title:
+"Hoppla, ein Fehler ist aufgetreten!",
+promocode_deleted_at_checkout:
+"Der von Ihnen verwendete Gutschein hat die Maximalzahl an Einlösungen erreicht, während Sie im Bezahlprozess waren. Bitte entschuldigen Sie diese Unannehmlichkeit.",
+continue_shopping:
+"Einkauf fortsetzen",
+payment_required_message:
+"Der Warenkorb wurde für diese Website deaktiviert. Falls Sie der Besitzer dieser Website sind, loggen Sie sich ins Snipcart Dashboard ein, um das Problem zu beheben.",
+payment_require_title:
+"Der Warenkorb ist deaktiviert.",
+configuration_problem:
+"Konfigurationsfehler",
+additionnal_information:
+"Schreiben Sie uns hier, wenn Sie uns Feedback bzw. mehr Information zu diesem Problem senden möchten.",
+send_error:
+"Diesen Fehler an den Webseiten-Administrator senden.",
+message_sent:
+"Die Nachricht wurde gesendet. Danke!",
+paypalexpress_loading:
+"Sie werden für die Zahlung zu Paypal weitergeleitet.",
+paypalexpress_cancelled:
+"Sie haben die Transaktion abgebrochen. Wenn Sie es nochmals versuchen wollen, klicken Sie auf den Knopf.",
+retry:
+"Nochmals versuchen",
+error_crawlingfailed_title:
+"Es gab einene Fehler bei der Validierung Ihrer Bestellung. Es wurde keine Zahlung ausgelöst.",
+error_crawling_unreachable:
+"Artikel <strong>{0}</strong> kann unter <strong>{1}</strong> nicht aufgerufen werden. Bitte stellen Sie sicher, dass die URL öffentlich erreichbar ist.",
+error_crawling_product_not_found:
+"Artikel <strong>{0}</strong> wurde unter <strong>{1}</strong> nicht gefunden.",
+error_crawling_price_not_found:
+"Artikel <strong>{0}</strong> hat keinen definierten Preis unter <strong>{1}</strong>. Definiere den Preis mit data-item-price.",
+error_crawling_price_doesnot_match:
+"Artikel <strong>{0}</strong> Preis unter <strong>{3}</strong> ist <strong>{2}</strong> , sollte aber <strong>{1}</strong> sein.",
+error_crawlingfailed_title_test:
+"Es gab einen Fehler bei der Validierung Ihrer Bestellung. Es wurde keine Zahlung ausgelöst. Diese Webseite ist derzeit im Testmodus.",
+order_completedon:
+"Bestellt am",
+payment_method_status:
+"Transaktionsstatus",
+payment_method_status_approved:
+"Bestätigt",
+order_reference_number:
+"Referenznummer",
+order_transaction_amount:
+"Bestellsumme",
+order_invoice_number:
+"Rechnungsnummer",
+order_authorization_code:
+"Autorisierungscode",
+item_is_being_added:
+"Artikel wird zum Warenkorb hinzugefügt …",
+order_completing_payment:
+"Bestellung wird durchgeführt …",
+calculating_shipping_fees:
+"Versandkosten werden berechnet …",
+saving:
+"Speichern …",
+loading:
+"Laden …",
+free_shipping:
+"Gratis Versand",
+cart_plans_name:
+"Plan",
+cart_plans_interval:
+"Intervall",
+cart_plans_quantity:
+"Anzahl",
+cart_plans_amount:
+"Betrag",
+cart_plans_total:
+"Summe",
+payable_now:
+"Jetzt fällig",
+upcoming_payment_for:
+"Abonnement",
+upcoming_payment_date:
+"Datum",
+upcoming_payment_amount:
+"Summe",
+upcoming_payment_subtotal:
+"Zwischensumme",
+upcoming_payments:
+"Nächste Zahlungen",
+order_date:
+"Datum",
+order_status:
+"Status",
+order_total:
+"Summe",
+orders_history:
+"Bestell-Historie",
+orders_history_no_orders:
+"Es wurden bisher keine Bestellungen durchgeführt.",
+orders_fetching_orders:
+"Lade Bestellungen …",
+user_nav_orders:
+"Meine Bestellungen",
+user_nav_subscriptions:
+"Meine Abonnements",
+user_nav_cart:
+"Mein Warenkorb",
+subscriptions_history:
+"Abonnement-Historie",
+subscription_name:
+"Name",
+subscription_amount:
+"Betrag",
+subscription_quantity:
+"Anzahl",
+subscription_total_amount:
+"Summe",
+subscriptions_fetching_subscriptions:
+"Lade Abonnements …",
+subscriptions_fetching_details:
+"Lade Abonnement Details …",
+subscription_no_invoices:
+"Für dieses Abonnement liegen noch keine Rechnungen vor.",
+subscription_plan_name:
+"Plan Name",
+subscription_plan_interval:
+"Plan Interval",
+subscription_invoice_number:
+"Nummer",
+subscription_invoice_date:
+"Datum",
+subscription_invoice_amount:
+"Betrag",
+subscription_invoice_item:
+"Position",
+subscription_invoice_info:
+"Rechnungsdaten",
+subscription_invoice_details:
+"Rechnungdetails",
+subscription_invoice_fetching_details:
+"Lade Rechnungdetails …",
+subscription_invoices_loading:
+"Lade Abonnement-Rechnungen …",
+subscription_invoices_list:
+"Rechnungs-Historie",
+item_out_of_stock:
+"Es tut uns leid, dieser Artikel ist momentan nicht vorrätig.",
+click_here_to_remove_it:
+"Klicken Sie hier um den Artikel aus dem Warenkorb zu entfernen.",
+outofstock_notification:
+"Es tut uns leid, Artikel in Ihrem Warenkorb sind nicht vorrätig. Bitte überprüfen Sie Ihre Bestellung. Keine Angst, es wurde noch nichts berechnet.",
+plan_amount_per_day:
+"{0} / Tag",
+plan_amount_per_week:
+"{0} / Woche",
+plan_amount_per_month:
+"{0} / Monat",
+plan_amount_per_year:
+"{0} / Jahr",
+plan_days_of_trial:
+"{0} Tage Probe",
+subscription_cancel_button:
+"Dieses Abonnement kündigen",
+subscription_cancel_confirmation:
+"Sind Sie sicher, dass Sie Ihr Abonnement kündigen möchten?",
+subscription_cancel_success:
+"Ihr Abonnement wurde erfolgreich gekündigt.",
+subscription_cancelled_notice:
+"Dieses Abonnement ist inaktiv. Es wurde am {0} gekündigt.",
+subscription_cancelledon:
+"Gekündigt am",
+paypal_express_checkout_link:
+"Mit Paypal bezahlen",
+paypal_express_checkout_title:
+"Mit Paypal bezahlen",
+paypal_express_checkout_explanation:
+"Klicken Sie auf diesen Button, wenn Sie die Bezahlung direkt über PayPal abwickeln möchten.",
+notifications_item_not_added_due_to_max_quantity:
+"Der Artikel wurde nicht hinzugefügt, weil Sie die maximal zulässige Anzahl überschreiten würden.",
+error_impossible_to_calculate_taxes:
+"Wir konnten keine Steuer für diese Bestellung berechnen. Das kann an einer ungültigen Adresse liegen. Bitte stellen Sie sicher, dass Ihre Adresse eine gültige Postleitzahl enthält."
+});
