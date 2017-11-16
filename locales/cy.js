243c6d23b2c82e31fafab6b3cac4d7de156fab79
llyfrsam <32550508+llyfrsam@users.noreply.github.com>
2017-11-16 09:55:17 +0000

    Added Cymraeg localization file

---
+++ locales/cy.js
@@ -0,0 +1,580 @@
+Snipcart.execute('registerLocale', 'cy', {
+yes:
+"Ie",
+no:
+"Na",
+print:
+"Argraffu",
+download_as_pdf:
+"Lawrlwytho fel PDF",
+checkout:
+"Talu",
+close:
+"Cau",
+first_name:
+"Enw cyntaf",
+name:
+"Enw",
+last_name:
+"Cyfenw",
+company_name:
+"Enw'r cwmni",
+share_by_email:
+"Rhannu drwy e-bost",
+email:
+"E-bost",
+password:
+"Cyfrinair",
+confirm_password:
+"Cadarnhau cyfrinair",
+ok:
+"OK",
+send:
+"Anfon",
+address_1:
+"Cyfeiriad Stryd 1",
+address_2:
+"Cyfeiriad Stryd 2",
+city:
+"Dinas / Tref",
+postal_code:
+"Côd Post",
+phone:
+"Ffôn",
+previous:
+"Cam blaenorol",
+next:
+"Cam nesaf",
+finalize:
+"Gosod archeb",
+country:
+"Gwlad",
+subtotal:
+"is-gyfanswm",
+rebate:
+"Ad-dalu",
+apply_promo_code:
+"Defnyddio’r côd",
+my_cart:
+"Fy masged",
+my_cart_content:
+"Cynnwys fy masged",
+shipping_method:
+"Dull cludiant",
+payment_method:
+"Dull talu",
+confirm_order:
+"Cadarnhau archeb",
+bill_me_later:
+"Talu'n hwyrach ymlaen",
+bill_me_later_action:
+"Talu'n hwyrach ymlaen",
+bill_me_later_explanation:
+"Anfonir anfoneb atoch trwy e-bost.",
+pay_via_mollie:
+"Dewiswch ddull talu",
+pay_now_via_mollie:
+"Talu",
+pay_via_mollie_explanation:
+"Byddwch yn cael eich ailgyfeirio at restr o'r opsiynau talu sydd ar gael.",
+promo_code_applied_successfully:
+"Mae eich gostyngiad wedi ei weithredu yn llwyddiannus.",
+promo_code_is_invalid:
+"Nid ywr côd hwn yn ddilys",
+promo_code_is_expired:
+"Mae'r côd hwn wedi dod i ben",
+promo_code_code:
+"Gostyngiad?",
+promo_code_rate_on_order:
+"gostyngiad ar yr archeb",
+promo_code_alternate_price:
+"pris arbennig ar gynnyrch",
+total:
+"Cyfanswm",
+total_paid:
+"Cyfanswm y taliad",
+province_state:
+"Sir / Talaith",
+billing_address:
+"Cyfeiriad anfoneb",
+shipping_address:
+"Cyfeiriad cludo",
+payment_informations:
+"Gwybodaeth Taliad",
+payment_informations_bill_me_later:
+"Rwyf am dalu yn hwyrach 'mlaen",
+payment_informations_no_payment_required:
+"Nid oes angen taliad",
+payment_informations_paypalexpress:
+"Talu gyda Paypal",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Ionawr",
+months_february:
+"Chwefror",
+months_march:
+"Mawrth",
+months_april:
+"Ebrill",
+months_may:
+"Mai",
+months_june:
+"Mehefin",
+months_july:
+"Gorffennaf",
+months_august:
+"Awst",
+months_september:
+"Medi",
+months_october:
+"Hydref",
+months_november:
+"Tachwedd",
+months_december:
+"Rhagfyr",
+cart_items_table_item:
+"Eitem",
+cart_items_table_description:
+"Disgrifiad",
+cart_items_table_quantity:
+"Nifer",
+cart_items_table_unit_price:
+"Pris Uned",
+cart_items_table_total_price:
+"Cyfanswm",
+cart_empty_text:
+"Mae'r fasged yn wag. Dewiswch gynnyrch i'w prynu cyn cwblhau archeb.",
+new_account_form_create_new_account:
+"Creu mewngofnod",
+new_account_form_create_new_account_action:
+"Creu mewngofnod",
+login_form_having_an_account:
+"Sign in",
+login_form_login_action:
+"Mewngofnodi",
+login_title:
+"Mewngofnodi",
+login_form_forgot_password_action:
+"Rwyf wedi anghofio fy nghyfrinair",
+forgot_password_forgot_your_password:
+"Ydych chi wedi anghofio eich cyfrinair?",
+forgot_password_please_enter_email:
+"Rhowch eich e-bost, byddwn yn anfon e-bost atoch sy'n cynnwys dolen unigryw i ailosod eich cyfrinair.",
+forgot_password_success_email_sent:
+"E-bost wedi ei anfon",
+forgot_password_email_sent_message:
+"Anfonwyd e-bost atoch gyda chyfarwyddiadau ynglŷn â sut i ailosod eich cyfrinair. Ewch i'ch e-bost a dilynwch y camau.",
+login_checkout_as_guest:
+"Talu fel ymwelydd",
+login_checkout_as_guest_notice:
+"Talwch fel ymwelydd os nad ydych am greu cyfrif gyda'r archeb. Ni fydd eich gwybodaeth yn cael ei gadw ar gyfer eich archebion nesaf.",
+shipping_address_same_as_billing:
+"Defnyddiwch y cyfeiriad hwn ar gyfer cludiant",
+shipping_method_method_name:
+"Dull cludiant",
+shipping_method_shipping_price:
+"Pris cludiant",
+shipping_method_failure_message:
+"Ni allwn ddarganfod dull cludiant. Gwnewch yn siŵr fod y cyfeiriad cludiant yn gywir os gwelwch yn dda.",
+shipping_method_failure_click_here_to_edit:
+"Cliciwch yma i olygu eich cyfeiriad cludiant",
+payment_method_card_holder:
+"Enw ar y cerdyn",
+payment_method_card_type:
+"Math o gerdyn",
+payment_method_card_number:
+"Rhif cerdyn",
+payment_method_card_cvc:
+"CVC",
+payment_method_card_exp_month:
+"Mis mae'r cerdyn yn dod i ben",
+payment_method_card_exp_year:
+"Blwyddyn mae'r cerdyn yn dod i ben",
+payment_method_cvc_infos:
+"Y CVC yw'r rhif diogelwch 3 digid sydd ar gefn y cerdyn credyd, fel arfer ar y dde i'ch llofnod.",
+payment_status:
+"Statws y taliad",
+create_an_account:
+"Creu mewngofnod",
+why_create_account:
+"I gyflymu eich archeb nesaf, rhowch gyfrinair i greu cyfrif.",
+reset_password:
+"Ailosod cyfrinair",
+reset_password_success:
+"Ailosod cyfrinair wedi ei gwblhau",
+reset_password_changed:
+"Mae eich cyfrinair wedi ei newid.",
+reset_password_click_here_to_login:
+"Cliciwch yma i fewngyfnodi",
+thankyou_message:
+"Diolch am eich archeb! Mae eich anfoneb wedi ei e-bostio i chi, dylech ei dderbyn yn fuan.",
+thankyou_submessage:
+"Byddwch yn derbyn e-bost i gadarnhau yn fuan",
+account_created_successfully:
+"Mae eich cyfrif wedi ei greu yn llwyddiannus",
+account_created_successfully_message:
+"Mae eich cyfrif wedi ei greu yn llwyddiannus. Diolch yn fawr iawn.",
+errors_required:
+"Mae hwn yn flwch gofynnol",
+errors_passwords_dont_match:
+"Mae'n rhaid i'r ddau gyfrinair gyfateb",
+errors_email_must_be_unique:
+"Mae defnyddiwr o'r un enw yn bodoli",
+errors_both_password_must_match:
+"Mae'n rhaid i'r ddau gyfrinair gyfateb",
+errors_email_must_be_valid:
+"Mae'n rhaid ir e-bost for yn ddilys",
+errors_email_does_not_match_any_existing_user:
+"Nid oes defnyddiwr gyda'r e-bost hwn yn bodoli",
+errors_email_does_not_match_reset_password_request:
+"Nid yw e-bost y defnyddiwr yn cyfateb i unrhyw gais i ailosod cyfrinair.",
+errors_reset_password_token_expired:
+"Mae'r amser i ailosod cyfrinair wedi do i ben.",
+errors_invalid_authentication_infos:
+"Gwybodaeth annilys",
+error_payment_items_empty:
+"Mae'n ymddangos fod eich archeb yn annilys, ail-lwythwch y dudalen os gwelwch yn dda. Nid oes taliad wedi ei wneud ar eich cerdyn credyd.",
+error_payment_items_are_invalid:
+"Nid ydym wedi llwyddo i gwblhau eich archeb. Mae'n ymddangos bod gan un o'r eitemau yn eich basged siopa bris anghywir.",
+error_crawling_failed:
+"Nid ydym wedi gallu dilysu eich archeb, ni chodwyd tâl ar eich cerdyn credyd, ceisiwch eto mewn ychydig funudau.",
+error_discounts_have_expired:
+"Yn anffodus, mae un o'r gostyngiadau wedi dod i ben cyn i chi gwblhau'r broses. Adolygwch eich archeb isod a cheisiwch eto.",
+error_shipping_price_outdated:
+"Nid yw'r opsiwn cludiant a ddewiswyd gennych yn ddilys bellach, dewiswch ddull arall.",
+powered_by:
+"Wedi'i bweru a'i ddiogelu gan",
+promocode_rate_format:
+"{0}% o ostyngiad ar eich archeb",
+promocode_amount_format:
+"{0} o ostyngiad ar eich archeb",
+shipping_method_business_days:
+"{0} dyddiau busnes",
+shipping_method_business_day:
+"{0} dyddiau busnes",
+shipping_method_delivery_time:
+"Erbyn {0}", // By 2013-11-28
+welcome:
+"Croeso",
+back:
+"Yn ôl",
+order_infos:
+"Gwybodaeth archeb",
+generic_error_title:
+"Wps, camgymeriad!",
+promocode_deleted_at_checkout:
+"Mae'r gostyngiad a ddefnyddiwyd gennych wedi dod i ben. Mae'n ddrwg gennym am yr anghyfleustra.",
+continue_shopping:
+"Parhau i siopa",
+payment_required_message:
+"Mae'r fasged siopa ar gyfer y wefan hon wedi ei chau. Os mai chi yw perchennog y wefan hon, mewngofnodwch i mewn i Snipcart i ddatrys y mater.",
+payment_require_title:
+"Mae'r fasged siopa wedi ei chau.",
+configuration_problem:
+"Problem ffurfweddu",
+additionnal_information:
+"Rhowch neges isod os ydych am anfon adborth neu ragor o wybodaeth am y broblem hon.",
+send_error:
+"Anfon y gwall hwn i berchenog y wefan",
+message_sent:
+"Neges wedi ei anfon, diolch",
+paypalexpress_loading:
+"Arhoswch ychydig eiliadau...",
+paypalexpress_cancelled:
+"Rydych wedi canslo'r pryniant, gallwch naill ai glicio ar y botwm isod i roi cynnig arall arni neu barhau i siopa.",
+retry:
+"Trïwch eto",
+error_crawlingfailed_title:
+"Aeth rhywbeth o'i le wrth ddilysu eich archeb, peidiwch â phoeni, nid oes taliad wedi ei wneud.",
+error_crawling_unreachable:
+"Mae eitem <strong>{0}</strong> yn anghyraeddadwy ar <strong>{1}</strong>. Gwnewch yn siŵr bod URL y cynnyrch yn 'publicly available'.",
+error_crawling_product_not_found:
+"Nid yw eitem <strong>{0}</strong> wedi ei ddarganfod ar <strong>{1}</strong>.",
+error_crawling_price_not_found:
+"Nid oes gan <strong>{0}</strong> bris penodol ar <strong>{1}</strong>, rhowch fanylion data-item-price.",
+error_crawling_price_doesnot_match:
+"Mae pris eitem <strong>{0}</strong> ar <strong>{3}</strong> yn <strong>{2}</strong> ond dylai fod yn <strong>{1}</strong>.",
+error_crawlingfailed_title_test:
+"Aeth rhywbeth o'i le wrth ddilysu eich archeb, peidiwch â phoeni, nid oes taliad wedi ei wneud. Mae'r wefan hon mewn 'Test mode'.",
+order_completedon:
+"Gosodwyd ar",
+payment_method_status:
+"Statws pryniant",
+payment_method_status_approved:
+"Cymeradwywyd",
+order_reference_number:
+"Rhif cyfeirnod",
+order_transaction_amount:
+"Swn pryniant",
+order_invoice_number:
+"Rhif anfoneb",
+order_authorization_code:
+"Côd awdurdodi",
+item_is_being_added:
+"Yn ychwanegu'r eitem i'r fasged...",
+order_completing_payment:
+"Yn gosod yr archeb...",
+calculating_shipping_fees:
+"Cyfrifo pris cludiant...",
+saving:
+"Yn cadw...",
+loading:
+"Llwytho...",
+free_shipping:
+"Cludiant am ddim",
+cart_plans_name:
+"Cynllun",
+cart_plans_interval:
+"Cyfnod",
+cart_plans_interval_count:
+"Cyfrifo",
+cart_plans_quantity:
+"Nifer",
+cart_plans_amount:
+"Swm",
+cart_plans_total:
+"Cyfanswm",
+payable_now:
+"Yn daladwy nawr",
+upcoming_payment_for:
+"Tanysgrifiad",
+upcoming_payment_date:
+"Dyddiad",
+upcoming_payment_amount:
+"Cyfanswm",
+upcoming_payment_subtotal:
+"is-Gyfanswm",
+upcoming_payment_shipping:
+"Cludiant",
+upcoming_payments:
+"Taliadau nesaf",
+order_date:
+"Dyddiad",
+order_status:
+"Statws",
+order_total:
+"Cyfanswm",
+orders_history:
+"Fy archebion",
+subscriptions_history_no_subscriptions:
+"Nid oes gennych danysgrifiadau hyd yma.",
+orders_history_no_orders:
+"Nid oes gennych archebion hyd yma.",
+orders_fetching_orders:
+"Llwytho eich archebion...",
+user_nav_orders:
+"Fy archebion",
+user_nav_subscriptions:
+"Fy nhanysgrifiadau",
+user_nav_cart:
+"Fy masged",
+subscriptions_history:
+"Fy nhanysgrifiadau",
+subscription_name:
+"Enw",
+subscription_amount:
+"Swm",
+subscription_quantity:
+"Nifer",
+subscription_total_amount:
+"Cyfanswm",
+subscriptions_fetching_subscriptions:
+"Llwytho tanysgrifiadau...",
+subscriptions_fetching_details:
+"Llwytho manylion tanysgrifiadau...",
+subscription_no_invoices:
+"Nid oes gan y tanysgrifiad unrhyw anfonebau eto.",
+subscription_plan_name:
+"Enw'r cynllun",
+subscription_plan_interval:
+"Cynllunio cyfnod",
+subscription_plan_interval_count:
+"Cynllunio cyfrif cyfnod",
+subscription_invoice_number:
+"Rhif",
+subscription_invoice_date:
+"Dyddiad",
+subscription_invoice_amount:
+"Swm",
+subscription_invoice_item:
+"Eitem",
+subscription_invoice_info:
+"Gwybodaeth anfoneb",
+subscription_invoice_details:
+"Manylion anfoneb",
+subscription_invoice_fetching_details:
+"Llwytho manylion anfoneb...",
+subscription_invoices_loading:
+"Llwytho tanysgrifiad anfonebau...",
+subscription_invoices_list:
+"Rhestr anfonebau",
+item_out_of_stock:
+"Mae'r eitem hwn allan o stoc, mae'n ddrwg iawn gennym.",
+click_here_to_remove_it:
+"Cliciwch yma iw dynnu o'r fasged.",
+outofstock_notification:
+"Aeth rhai eitemau yn eich basged allan o stoc tra roeddech yn prynu. Adolygwch eich archeb os gwelwch yn dda. Peidiwch â phoeni, nid oes taliad wedi ei wneud.",
+plan_amount_per_day:
+"{0} / diwrnod",
+plan_amount_per_week:
+"{0} / wythnos",
+plan_amount_per_month:
+"{0} / mis",
+plan_amount_per_year:
+"{0} / blwyddyn",
+plan_amount_per_day_plural:
+"{0} / {1} dyddiau",
+plan_amount_per_week_plural:
+"{0} / {1} wythnosau",
+plan_amount_per_month_plural:
+"{0} / {1} misoedd",
+plan_amount_per_year_plural:
+"{0} / {1} blynyddoedd",
+plan_days_of_trial:
+"{0} diwrnod i dreialu",
+subscription_cancel_button:
+"Canslo",
+subscription_pause_button:
+"Oedi",
+subscription_cancel_confirmation:
+"Ydych chi'n siŵr eich bod am ganslo'ch tanysgrifiad?",
+subscription_cancel_success:
+"Mae eich tanysgrifiad wedi'i ganslo'n llwyddiannus.",
+subscription_cancelled_notice:
+"Nid yw'r tanysgrifiad hwn bellach yn weithredol, mae wedi'i ganslo ar {0}.",
+subscription_cancelledon:
+"Wedi'i ganslo ar",
+paypal_express_checkout_link:
+"Talu gyda Paypal",
+paypal_express_checkout_title:
+"Talu gyda PayPal",
+paypal_express_checkout_explanation:
+"Cliciwch ar y botwm hwn os yw'n well gennych brosesu eich taliad yn uniongyrchol trwy PayPal.",
+notifications_item_not_added_due_to_max_quantity:
+"Nid yw'r cynnyrch wedi ei ychwanegu, mae gennych chi'r uchafswm yn eich basged.",
+error_impossible_to_calculate_taxes:
+"Nid ydym wedi gallu cyfrifo trethi ar gyfer yr archeb, efallai bod hyn oherwydd bod cyfeiriad annilys, gwnewch yn siŵr eich bod wedi rhoi cyfeiriad dilys gyda Côd ZIP 5 digid.",
+have_a_promocode_question:
+"Côd gostyngiad?",
+order_totals_computing:
+"Cyfrifo trethi...",
+order_validation_failed:
+"Nid ydym wedi gallu dilysu eich archeb. Mae'n debygol prisiau rhai o'r cynnyrch wedi newid ers i chi eu rhoi yn y fasged. Adolygwch eich archeb a cheisiwch eto.",
+item_invalid_must_remove:
+"Mae'n edrych fel nad yw'r eitem hwn ar gael mwyach. Efallai y bydd angen i chi gysylltu â'r masnachwr i ddatrys hyn. Trwy barhau, bydd yr eitem hwn yn cael ei dynnu o'ch basged.",
+accept_cart_changes:
+"Derbyn newidiadau a pharhau",
+payment_failed_text:
+"Mae'n ddrwg gennym; nid ydym wedi gallu prosesu'ch taliad. Gallwch barhau i siopa neu geisio eto gan ddefnyddio'r botymau isod.",
+payment_method_willbepaidlater:
+"Gohirio",
+payment_method_paypal:
+"Paypal",
+payment_method_none:
+"Dim",
+payment_method_sofort:
+"Sofort",
+payment_method_ideal:
+"Ideal",
+payment_method_mistercash:
+"Mister Cash",
+payment_method_banktransfer:
+"Trosglwyddiad banc",
+payment_method_directdebit:
+"Credyd uniongyrchol",
+payment_method_belfius:
+"Belfius",
+payment_method_bitcoin:
+"Bitcoin",
+payment_method_podiumcadeaukaart:
+"Podium Cadeau Kaart",
+payment_method_paysafecard:
+"PaySafe card",
+payment_method_bancontact:
+"Bancontact",
+payment_method_creditcard:
+"Cerdyn Credyd",
+error_item_stock_exceeded:
+"Mae'n ddrwg gennym! Mae'ch cais yn fwy na'r stoc sydd ar gael ar gyfer yr eitem hwn.",
+error_item_out_of_stock_text:
+"Mae'n ddrwg gennym! Nid yw'n edrych fel bod y cynnyrch hwn ar gael bellach. Triwch eto cyn bo hir.",
+item_out_of_stock_with_variant:
+"Mae'n ddrwg gennym! Nid yw'r fersiwn hwn o'r cynnyrch ar gael mwyach. Dewiswch fersiwn arall neu bydd yr eitem hwn yn cael ei dynnu o'r fasged siopa.",
+order_status_processed:
+"Wedi ei brosesu",
+order_status_disputed:
+"Anghydfod",
+order_status_shipped:
+"Cludwyd",
+order_status_delivered:
+"Derbyniwyd",
+order_status_pending:
+"Disgwyl",
+order_status_cancelled:
+"Wedi ei ganslo",
+something_went_wrong_while_fetching_rates:
+"Nid ydym wedi gallu dod o hyd i unrhyw ddulliau cludinat ar gyfer y cyfeiriad a bennwyd gennych. Cysylltwch â ni yn uniongyrchol os oes angen help arnoch.",
+error_must_select_shipping_rate:
+"Rhaid i chi ddewis dull cludiant i barhau.",
+back_to_orders:
+"Ôl archebion",
+manage_subscriptions:
+"Rheoli tanysgrifiadau",
+back_to_subscriptions_list:
+"Yn ôl i'r rhestr tanysgrifiadau",
+back_to_subscription_details:
+"Yn ôl i fanylion tanysgrifiad",
+discount_remove_confirmation_msg:
+"Ydych chi'n siŵr eich bod am gael gwared o'r gostyngiad?",
+digital_files_not_found:
+"Nid oes un neu fwy o ffeiliau digidol yn bodoli, cysylltwch â pherchennog y siop am ragor o fanylion.",
+digital_files_download_button:
+"Lawrlwytho ffeil",
+notifications_item_not_modified_due_to_min_quantity:
+"Mae'n ddrwg gennym, mae angen archebu mwy nag {0} o'r cynnyrch hwn.",
+subscription_resume_buttom:
+"Parhau",
+subscription_summary:
+"Crynhodeb",
+subscription_notifications_paused:
+"Mae'r tanysgrifiad hwn wedi ei stopio ar hyn o bryd, gallwch chi ei ailddechrau trwy glicio ar y botwm Ailgychwyn.",
+subscription_notifications_pause_confirm:
+"A ydych yn siŵr eich bod eisiau torri'ch tanysgrifiad?",
+subscription_notifications_resume_confirm:
+"A ydych yn siŵr eich bod eisiau parhau a'ch tanysgrifiad?",
+subscription_status_canceled:
+"Wedi ei ganslo",
+subscription_status_paused:
+"Wedi ei oedi",
+refund_line:
+"Ad-dalwyd",
+stripe_invalid_number:
+"Nid yw'r rhif cerdyn credyd yn ddilys.",
+stripe_invalid_expiry_month:
+"Mae'r mis dod i ben y cerdyn yn annilys.",
+stripe_invalid_expiry_year:
+"Mae'r flwyddyn dod i ben y cerdyn yn annilys.",
+stripe_invalid_cvc:
+"Mae côd diogelwch y cerdyn yn annilys.",
+stripe_invalid_swipe_data:
+"Mae data llithro y cerdyn yn annilys.",
+stripe_incorrect_number:
+"Mae rhif y cerdyn yn anghywir.",
+stripe_expired_card:
+"Mae'r cerdyn wedi dod i ben.",
+stripe_incorrect_cvc:
+"Mae côd diogelwch y cerdyn yn anghywir.",
+stripe_incorrect_zip:
+"Mae cod post / zip y cerdyn wedi methu cael ei ddilysu.",
+stripe_card_declined:
+"Gwrthodwyd y cerdyn.",
+stripe_missing:
+"Nid oes cerdyn ar gwsmer sy'n gwneud pryniant .",
+stripe_processing_error:
+"Digwyddodd gwall wrth brosesu'r cerdyn"
+});
