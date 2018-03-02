11a3a94978dcb8f415eddba54dd77aae3f078476
Patrik Illy <patrik.illy@gmail.com>
2018-03-02 15:10:01 +0100

    Add czech translation

---
+++ locales/cs-CZ.js
@@ -0,0 +1,514 @@
+Snipcart.execute('registerLocale', 'cs-CZ', {
+yes:
+"Ano",
+no:
+"Ne",
+print:
+"Tisk",
+download_as_pdf:
+"Stáhnout jako PDF",
+checkout:
+"Zaplatit",
+close:
+"Zavřít",
+first_name:
+"Jméno",
+name:
+"Příjmení",
+last_name:
+"Příjmení",
+company_name:
+"Firma",
+share_by_email:
+"Sdílet",
+email:
+"Email",
+password:
+"Heslo",
+confirm_password:
+"Potvrdit heslo",
+ok:
+"OK",
+send:
+"Poslat",
+address_1:
+"Adresa",
+address_2:
+" ",
+city:
+"Město",
+postal_code:
+"PSČ",
+phone:
+"Telefon",
+previous:
+"Zpět",
+next:
+"Další",
+finalize:
+"Dokončit objednávku",
+country:
+"Země",
+subtotal:
+"Mezisoučet",
+rebate:
+"Sleva",
+apply_promo_code:
+"Použít promo kód",
+my_cart:
+"Můj košík",
+my_cart_content:
+"Obsah košíku",
+shipping_method:
+"Způsob dopravy",
+payment_method:
+"Způsob platby",
+confirm_order:
+"Potvrdit objednávku",
+bill_me_later:
+"Platba při převzetí",
+bill_me_later_action:
+"Potvrdit",
+bill_me_later_explanation:
+"Potvrzení objednávky vám přijde mailem.",
+pay_via_mollie:
+"Vyberte způsob platby",
+pay_now_via_mollie:
+"Zaplatit",
+pay_via_mollie_explanation:
+"Budete přesměrováni na výber zůsobu platby",
+promo_code_applied_successfully:
+"Vaše sleva byla úspěšně aktivována.",
+promo_code_is_invalid:
+"Tento kód je nesprávný.",
+promo_code_is_expired:
+"Tento kód vypršel.",
+promo_code_code:
+"",
+promo_code_rate_on_order:
+"sleva na objednávku",
+promo_code_alternate_price:
+"speciální cena produktů",
+total:
+"Celkem",
+total_paid:
+"Celková cena objednávky",
+province_state:
+"Okres",
+billing_address:
+"Vaše údaje",
+shipping_address:
+"Dodací adresa",
+payment_informations:
+"Platba",
+payment_informations_bill_me_later:
+"Zaplatím později",
+payment_informations_no_payment_required:
+"Platba není vyžadována",
+payment_informations_paypalexpress:
+"platba přes Paypal",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Leden",
+months_february:
+"Únor",
+months_march:
+"Březen",
+months_april:
+"Duben",
+months_may:
+"Květen",
+months_june:
+"Červen",
+months_july:
+"Červenec",
+months_august:
+"Srpen",
+months_september:
+"Září",
+months_october:
+"Říjen",
+months_november:
+"Listopad",
+months_december:
+"Prosinec",
+cart_items_table_item:
+"Zboží",
+cart_items_table_description:
+"Popis",
+cart_items_table_quantity:
+"Množství",
+cart_items_table_unit_price:
+"Cena za kus",
+cart_items_table_total_price:
+"Celková cena",
+cart_empty_text:
+"Váš košík je prázdný",
+new_account_form_create_new_account:
+"Založit účet",
+new_account_form_create_new_account_action:
+"Založit účet",
+login_form_having_an_account:
+"Přihlásit se",
+login_form_login_action:
+"Přihlásit se",
+login_title:
+"Přihlášení",
+login_form_forgot_password_action:
+"Zapomněl jsem heslo",
+forgot_password_forgot_your_password:
+"Zapomenuté heslo?",
+forgot_password_please_enter_email:
+"Prosím zadejte svůj email, zašleme vám mail s odkazem na resetování vašeho hesla",
+forgot_password_success_email_sent:
+"Email odeslán",
+forgot_password_email_sent_message:
+"Byl vám odeslán email s instrukcemi jak vyresetovat heslo. Podívejte se na mail a následujte kroky.",
+login_checkout_as_guest:
+"Zaplatit bez registrace",
+login_checkout_as_guest_notice:
+"Zaplatit bez registrace pokud nechcete pro tento nákup vytvářet účet. Pamatujte, že vaše informace nebudou uložena pro příští objednávky",
+shipping_address_same_as_billing:
+"Uvedená adresa je zároveň adresou dodaní",
+shipping_method_method_name:
+"Způsob dopravy",
+shipping_method_shipping_price:
+"Cena dopravy",
+shipping_method_failure_message:
+"Nebyli jsme schopní ověřit Vaši objednávku. Vypadá to, že některý z produktů ve vašem košíku nemá správně nastavené parametry.",
+shipping_method_failure_click_here_to_edit:
+"Klikněte zde pro změnu vaší dodací adresy",
+payment_method_card_holder:
+"Jméno na kartě",
+payment_method_card_type:
+"Tuto záložku přeskočte / Typ karty",
+payment_method_card_number:
+"----r",
+payment_method_card_cvc:
+"---",
+payment_method_card_exp_month:
+"----",
+payment_method_card_exp_year:
+"----",
+payment_method_cvc_infos:
+"CVC je 3-místný bezpečností kód na zadní straně vaší kreditní karty, obvykle bývá za podpisem.",
+payment_status:
+"Stav platby",
+create_an_account:
+"Zaregistrovat",
+why_create_account:
+"Pro rychlejší dokončení další objednávky, zadejte prosím heslo a zaregistrujte se.",
+reset_password:
+"Resetovat heslo",
+reset_password_success:
+"Resetování hesla dokončeno",
+reset_password_changed:
+"Vaše heslo bylo změněno.",
+reset_password_click_here_to_login:
+"Pro přihlášení klikněte zde",
+thankyou_message:
+"Děkujeme vám za vaši objednávku!",
+thankyou_submessage:
+"Brzy obdržíte potvrzovací email",
+account_created_successfully:
+"Účet byl úspěšně založen.",
+account_created_successfully_message:
+"Váš účet byl úspěšně vytvořen, děkuji.",
+errors_required:
+"Povinný údaj",
+errors_email_must_be_unique:
+"Uživatel se stejnou emailovou adresou již existuje",
+errors_both_password_must_match:
+"Hesla se musí shodovat",
+errors_email_must_be_valid:
+"Email musí být platný",
+errors_email_does_not_match_any_existing_user:
+"Uživatel s tímto mailem neexistuje",
+errors_email_does_not_match_reset_password_request:
+"Uživatelský mail neodpovídá žádnému existujícímu požadavku o reset hesla.",
+errors_reset_password_token_expired:
+"Žádost o reset hesla vypršela.",
+errors_invalid_authentication_infos:
+"Špatné autorizační informace",
+error_payment_items_empty:
+"Vypadá, že vaše není v pořádku, prosím načtěte znovu stránku. Částka vám nebyla stržena.",
+error_payment_items_are_invalid:
+"Nebyli jsme schopni dokončit vaši objednávku. Vypadá to, že některá z položek v košíku nemá platnou cenu.",
+error_crawling_failed:
+"Nemohli jsme potvrdit objednávku, částka nebyla stržena, prosím zkuste to znovu za chvíli.",
+error_discounts_have_expired:
+"Bohužel sleva vypršela předtím než jste dokončili objednávku, zkotrolujte ji prosím a zkuste to znovu.",
+powered_by:
+"Powered and secured by",
+promocode_rate_format:
+"{0}% sleva na vaši objednávku",
+promocode_amount_format:
+"{0} sleva na vaši objednávku",
+shipping_method_business_days:
+"{0}-3 pracovních dní",
+shipping_method_business_day:
+"{0} pracovní den",
+shipping_method_delivery_time:
+"Dne {0}", // By 2013-11-28
+welcome:
+"Vítejte",
+back:
+"Zpět",
+order_infos:
+"Informace o objednávce",
+generic_error_title:
+"Ajaj, došlo k chybě.",
+promocode_deleted_at_checkout:
+"Počet použítí slevy byl vyčerpán. Omlouváme se za potíže",
+continue_shopping:
+"Pokračovat v nákupu",
+payment_required_message:
+"Nákupní košík pro tuto stránku byl zakázán. Pokud jste majitelem stránky, přihlašte se do snipcartu a chybu opravte.",
+payment_require_title:
+"Nákupní košík je zakázán",
+configuration_problem:
+"Problém s konfigurací",
+additionnal_information:
+"Zadejte dolů zprávu pokud chcete odeslat zpětnou vazbu nebo více informací o tomto problému.",
+send_error:
+"Zašlete tuto chybu majiteli stránky",
+message_sent:
+"Zpráva odeslána, děkujeme.",
+paypalexpress_loading:
+"Počejte chvilku...",
+paypalexpress_cancelled:
+"Zrušili jste transakci, zkuste to znovu kliknutím na tlačítko dole nebo pokračujte v nákupu..",
+retry:
+"Zkusit znovu",
+error_crawlingfailed_title:
+"Něco se pokazilo při ověřeování vaší objednávky, nebojte, peníze nebyly strženy.",
+error_crawling_unreachable:
+"Položka <strong>{0}</strong> není dostupná na <strong>{1}</strong>. Ujistěte se že je URL adresa veřejně dostupná.",
+error_crawling_product_not_found:
+"Položka <strong>{0}</strong> nebyla naleza na <strong>{1}</strong>.",
+error_crawling_price_not_found:
+"Položka <strong>{0}</strong> nemá zadanou cenu <strong>{1}</strong>, zadejte data-item-price.",
+error_crawling_price_doesnot_match:
+"Položka <strong>{0}</strong> za cenu <strong>{3}</strong> je <strong>{2}</strong> ale neměla by <strong>{1}</strong>.",
+error_crawlingfailed_title_test:
+"Something went wrong when validating your order, don't worry, you have not been charged. This website is currently in Test mode.",
+order_completedon:
+"Objednáno dne",
+payment_method_status:
+"Stav platby",
+payment_method_status_approved:
+"Schváno",
+order_reference_number:
+"Referenční číslo",
+order_transaction_amount:
+"Částka",
+order_invoice_number:
+"Číslo faktury",
+order_authorization_code:
+"Autorizační kód",
+item_is_being_added:
+"Vkládám zboží do košíku...",
+order_completing_payment:
+"Objednávám...",
+calculating_shipping_fees:
+"Přejít na dopravu...",
+saving:
+"Ukládám...",
+loading:
+"Načítám...",
+free_shipping:
+"Doprava zdarma",
+cart_plans_name:
+"Plán",
+cart_plans_interval:
+"Období",
+cart_plans_interval_count:
+"Počet",
+cart_plans_quantity:
+"Počet",
+cart_plans_amount:
+"Částka",
+cart_plans_total:
+"Celkem",
+payable_now:
+"Splatné ihned",
+upcoming_payment_for:
+"Předplatné",
+upcoming_payment_date:
+"Datum",
+upcoming_payment_amount:
+"Celkem",
+upcoming_payment_subtotal:
+"Mezisoučet",
+upcoming_payments:
+"Další platby",
+order_date:
+"Datum",
+order_status:
+"Stav",
+order_total:
+"Celkem",
+orders_history:
+"Moje objednávky",
+orders_history_no_orders:
+"Nemáte žádné objednávky.",
+orders_fetching_orders:
+"Načítám objednávky...",
+user_nav_orders:
+"Moje objednávky",
+user_nav_subscriptions:
+"My subscriptions",
+user_nav_cart:
+"Můj košík",
+subscriptions_history:
+"My subscriptions",
+subscription_name:
+"Jméno",
+subscription_amount:
+"Částka",
+subscription_quantity:
+"Množství",
+subscription_total_amount:
+"Celkem:",
+subscriptions_fetching_subscriptions:
+"Loading subscriptions...",
+subscriptions_fetching_details:
+"Loading subscrition details...",
+subscription_no_invoices:
+"This subscription does not have any invoices yet.",
+subscription_plan_name:
+"Jméno plánu",
+subscription_plan_interval:
+"Plan interval",
+subscription_plan_interval_count:
+"Plán interval počítať",
+subscription_invoice_number:
+"Číslo",
+subscription_invoice_date:
+"Datum",
+subscription_invoice_amount:
+"Částka",
+subscription_invoice_item:
+"Předmět",
+subscription_invoice_info:
+"Informace o faktuře",
+subscription_invoice_details:
+"Detaily faktury",
+subscription_invoice_fetching_details:
+"Načítám detaily faktury...",
+subscription_invoices_loading:
+"Loading subscription invoices...",
+subscription_invoices_list:
+"Historie faktur",
+item_out_of_stock:
+"Je nám líto ale předmět je vyprodaný",
+click_here_to_remove_it:
+"Klikněte zde pro odstanění předmětu z košíku",
+outofstock_notification:
+"je nám líto ale některý z předmětů se vyprodal během dokončování vaší objednávky. Zkontrolujte ji, prosím. Nebojte, peníze nebyly strženy.",
+plan_amount_per_day:
+"{0} / den",
+plan_amount_per_week:
+"{0} / týden",
+plan_amount_per_month:
+"{0} / mesíc",
+plan_amount_per_year:
+"{0} / rok",
+plan_amount_per_day_plural:
+"{0} / {1} dní",
+plan_amount_per_week_plural:
+"{0} / {1} týdnů",
+plan_amount_per_month_plural:
+"{0} / {1} mesíců",
+plan_amount_per_year_plural:
+"{0} / {1} roků",
+plan_days_of_trial:
+"{0} dnů zkušebně",
+subscription_cancel_button:
+"Zrušit předplatné",
+subscription_cancel_confirmation:
+"Are you sure you want to cancel your subscription ?",
+subscription_cancel_success:
+"Your subscription has been cancelled successfully.",
+subscription_cancelled_notice:
+"This subscription is no longer active, it has been cancelled on {0}.",
+subscription_cancelledon:
+"Zrušeno dne",
+paypal_express_checkout_link:
+"Zaplatit přes Paypal",
+paypal_express_checkout_title:
+"Zaplatit přes PayPal",
+paypal_express_checkout_explanation:
+"Klikněte na toto tlačítko pokud preferujete zaplatit přímo přes PayPal",
+notifications_item_not_added_due_to_max_quantity:
+"Produkt nebyl do košíku přídán, již v něm máte max. možný počet konkrétního produktu.",
+error_impossible_to_calculate_taxes:
+"We have not been able to calculate taxes for the order, it might be due to an invalid address, please ensure you entered a valid address with a 5 digits ZIP code.",
+have_a_promocode_question:
+"Slevový kód?",
+order_totals_computing:
+"Počítám daň...",
+order_validation_failed:
+"We have not been able to validate your order. Looks like some product prices might have changed since you added them to the cart. Please review your order and try again.",
+item_invalid_must_remove:
+"Vypadá to tak, že tato položka momentálně není k dispozicii. Problém je pravděpodobne v konfig. systému, ne v dostupnosti. Pro odstránění problému, prosím, kontaktujte nás mailem na david@3dsimo.com",
+accept_cart_changes:
+"Přijmout změny a pokračovat",
+payment_failed_text:
+"Omlouváme se ale nebylo možné strhnout peníze za objednávku. Pokračujte v nákupu nebo to zkuste znovu pomocí tlačítek dole.",
+payment_method_willbepaidlater:
+"Opožděná",
+payment_method_paypal:
+"Paypal",
+payment_method_none:
+"Žádná",
+payment_method_sofort:
+"Sofort",
+payment_method_ideal:
+"Ideal",
+payment_method_mistercash:
+"Mister Cash",
+payment_method_banktransfer:
+"Bankovní převod",
+payment_method_directdebit:
+"Direct debit",
+payment_method_belfius:
+"Belfius",
+payment_method_bitcoin:
+"Bitcoin",
+payment_method_podiumcadeaukaart:
+"Podium Cadeau Kaart",
+payment_method_paysafecard:
+"PaySafe card",
+payment_method_bancontact:
+"Bancontact",
+payment_method_creditcard:
+"Kreditní karta",
+error_item_stock_exceeded:
+"Omlouváme se, ale vámi požadované množství přesahuje počet produktů skladem.",
+error_item_out_of_stock_text:
+"Omlouvám se ale vypadá to, že produkt již není dostupný. Zkuste to prosím později.",
+item_out_of_stock_with_variant:
+"Omlouváme se ale vypadá to, že vybraná varianta produktu již není k dispozici. Vyberte si prosím jinou nebo ji odstraňte z košíku.",
+order_status_processed:
+"Zpracována",
+order_status_disputed:
+"Odmítnutá",
+order_status_shipped:
+"Odeslána",
+order_status_delivered:
+"Doručena",
+order_status_pending:
+"Čekající",
+order_status_cancelled:
+"Zrušena",
+something_went_wrong_while_fetching_rates:
+"Něco se pokazilo při počítání dopravného pro vaši objednávku. Zkontrolujte zda jste zadali platnou adresu. Chyba je: ",
+error_must_select_shipping_rate:
+"Pro pokračování musíte vybrat metodu dopravy."
+});
