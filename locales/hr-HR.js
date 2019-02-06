952911d7e7f0d5d175cb2d3a779e8de1fe67b4aa
Josip Pavlović <josip.codeart@outlook.com>
2019-02-06 21:55:22 +0100

    Added Croatian localization file

---
+++ locales/hr-HR.js
@@ -0,0 +1,630 @@
+﻿Snipcart.execute('registerLocale', 'hr-HR', {
+yes:
+"Da",
+no:
+"Ne",
+print:
+"Ispis",
+download_as_pdf:
+"Preuzmite kao PDF",
+checkout:
+"Provjera",
+close:
+"Zatvori",
+first_name:
+"Ime",
+name:
+"Ime",
+last_name:
+"Prezime",
+company_name:
+"Naziv tvrtke",
+share_by_email:
+"Podijelite preko e-maila",
+email:
+"Email",
+password:
+"Lozinka",
+confirm_password:
+"Potvrdite lozinku",
+ok:
+"OK",
+send:
+"Pošalji",
+address_1:
+"Adresa",
+address_2:
+"Adresa 2",
+city:
+"Grad",
+postal_code:
+"Poštanski broj",
+phone:
+"Telefon",
+previous:
+"Prethodni korak",
+next:
+"Sljedeći korak",
+finalize:
+"Naručiti",
+country:
+"Država",
+subtotal:
+"Ukupno",
+rebate:
+"Popust",
+apply_promo_code:
+"Primijenite promo kod",
+my_cart:
+"Moja košarica",
+my_cart_content:
+"Sadržaj košarice",
+shipping_method:
+"Način dostave",
+payment_method:
+"Način plaćanja",
+confirm_order:
+"Potvrda narudžbe",
+bill_me_later:
+"Naplati mi kasnije",
+bill_me_later_action:
+"Plati kasnije",
+bill_me_later_explanation:
+"Račun će biti poslan na email.",
+pay_via_mollie:
+"Odaberite metodu plaćanja",
+pay_now_via_mollie:
+"Plati sada",
+pay_via_mollie_explanation:
+"Bit ćete preusmjereni na popis dostupnih opcija plaćanja.",
+promo_code_applied_successfully:
+"Vaš popust je uspješno primijenjen.",
+promo_code_is_invalid:
+"Promotivni kod nije valjan.",
+promo_code_is_expired:
+"Ovaj promo kod je istekao.",
+promo_code_code:
+"Imate popust?",
+promo_code_rate_on_order:
+"Popust na narudžbu",
+promo_code_alternate_price:
+"posebna cijena za proizvod",
+total:
+"Ukupno",
+total_paid:
+"Ukupno plaćeno",
+province_state:
+"Županija",
+billing_address:
+"Adresa za naplatu",
+shipping_address:
+"Adresa za dostavu",
+payment_informations:
+"Informacije o plaćanju",
+payment_informations_bill_me_later:
+"Platit ću kasnije",
+payment_informations_no_payment_required:
+"Nije potrebno plaćanje",
+payment_informations_paypalexpress:
+"Plaćanje putem Paypala",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Siječanj",
+months_february:
+"Veljača",
+months_march:
+"Ožujak",
+months_april:
+"Travanj",
+months_may:
+"Svibanj",
+months_june:
+"Lipanj",
+months_july:
+"Srpanj",
+months_august:
+"Kolovoz",
+months_september:
+"Rujan",
+months_october:
+"Listopad",
+months_november:
+"Studeni",
+months_december:
+"Prosinac",
+cart_items_table_item:
+"Proizvod",
+cart_items_table_description:
+"Opis",
+cart_items_table_quantity:
+"Količina",
+cart_items_table_unit_price:
+"Cijena po komadu",
+cart_items_table_total_price:
+"Ukupna cijena",
+cart_empty_text:
+"Košarica je trenutno prazna. Odaberite proizvode koje želite kupiti prije provjere.",
+new_account_form_create_new_account:
+"Kreirajte račun",
+new_account_form_create_new_account_action:
+"Kreirajte račun",
+login_form_having_an_account:
+"Registrirajte se",
+login_form_login_action:
+"Prijavite se",
+login_title:
+"Prijava",
+login_form_forgot_password_action:
+"Zaboravio / la sam lozinku",
+forgot_password_forgot_your_password:
+"Zaboravili ste lozinku?",
+forgot_password_please_enter_email:
+"Molimo unesite Vaš email, poslat ćemo Vam email poruku koja sadrži jedinstveni link za obnovu lozinke.",
+forgot_password_success_email_sent:
+"Email poslan",
+forgot_password_email_sent_message:
+"Poslana Vam je email poruka s uputama kako obnoviti Vašu lozinku. Provjerite Vašu email poštu i slijedite korake.",
+login_checkout_as_guest:
+"Nastavi kao gost",
+login_checkout_as_guest_notice:
+"Nastavi kao gost ako ne želite izraditi račun za ovu kupnju. Napominjemo da vaše informacije neće biti spremljene za sljedeće narudžbe.",
+shipping_address_same_as_billing:
+"Koristite ovu adresu za dostavu",
+shipping_method_method_name:
+"Način dostave",
+shipping_method_shipping_price:
+"Cijena dostave",
+shipping_method_failure_message:
+"Nismo uspjeli pronaći niti jedan mogući način dostave. Molimo provjerite je li adresa za dostavu ispravna i pokušajte ponovno.",
+shipping_method_failure_click_here_to_edit:
+"Kliknite ovdje da biste promijenili adresu za dostavu",
+payment_method_card_holder:
+"Ime na kartici",
+payment_method_card_type:
+"Vrsta kartice",
+payment_method_card_number:
+"Broj kartice",
+payment_method_card_cvc:
+"CVC",
+payment_method_card_exp_month:
+"Mjesec isteka",
+payment_method_card_exp_year:
+"Godina isteka",
+payment_method_cvc_infos:
+"CVC je sigurnosni broj od tri znamenke na pozadini Vaše kreditne kartice, obično na desnoj strani od Vašeg potpisa.",
+payment_method_card_exp_date:
+"Datum isteka",
+payment_method_card_postal_code:
+"Poštanski broj",
+payment_status:
+"Status plaćanja",
+create_an_account:
+"Kreirajte račun",
+why_create_account:
+"Za brže preuzimanje sljedeće narudžbe unesite lozinku za izradu računa.",
+reset_password:
+"Resetiraj lozinku",
+reset_password_success:
+"Uspješno resetirana lozinka",
+reset_password_changed:
+"Vaša lozinka je promijenjena.",
+reset_password_click_here_to_login:
+"Kliknite ovdje za prijavu",
+thankyou_message:
+"Hvala na Vašoj Narudžbi! Vaša račun je poslan na Vaš email, koji bi uskoro trebali primiti.",
+thankyou_submessage:
+"Uskoro ćete dobiti potvrdni email",
+account_created_successfully:
+"Račun je uspješno kreiran",
+account_created_successfully_message:
+"Vaš račun je uspješno kreiran, hvala.",
+errors_required:
+"Ovo polje obavezno",
+errors_passwords_dont_match:
+"Obje zaporke se moraju podudarati",
+errors_email_must_be_unique:
+"Korisnik s istom email adresom već postoji",
+errors_both_password_must_match:
+"Obje zaporke se moraju podudarati",
+errors_email_must_be_valid:
+"Email mora biti valjan",
+errors_email_does_not_match_any_existing_user:
+"Ne postoji korisnik s ovom email adresom",
+errors_email_does_not_match_reset_password_request:
+"Korisnička email adresa ne odgovara niti jednom postojećem zahtjevu za obnavljanje zaporke.",
+errors_reset_password_token_expired:
+"Token za poništavanje lozinke je istekao.",
+errors_invalid_authentication_infos:
+"Nevažeće informacije o autentifikaciji",
+error_payment_items_empty:
+"Čini se da je vaša narudžba nevažeća, molimo ponovno učitajte stranicu. Vaša kreditna kartica nije terećena.",
+error_email_is_empty:
+"Izgleda da imamo problem sa spremanjem vaše email adrese, molimo unesite ponovo i kliknite dalje za nastavak.",
+error_payment_items_are_invalid:
+"We have not been able to complete your order. It seems that one of the items in your cart has an invalid price.",
+error_crawling_failed:
+"Nismo uspjeli dovršiti vašu narudžbu. Čini se da jedna od stavki u vašoj košarici ima nevažeću cijenu.",
+error_discounts_have_expired:
+"Nažalost jedan od popusta koji imate je istekao prije nego što ste dovršili postupak kupnje. Molimo pregledajte niže navedenu narudžbu i pokušajte ponovno.",
+error_shipping_price_outdated:
+"Opcija dostave koju ste odabrali više nije važeća, molimo odaberite drugi način dostave.",
+powered_by:
+"Omogućeno i osigurano pomoću",
+promocode_rate_format:
+"{0}% popust na Vašu narudžbu",
+promocode_amount_format:
+"{0} popust na Vašu narudžbu",
+shipping_method_business_days:
+"{0} radnih dana",
+shipping_method_business_day:
+"{0} radni dan",
+shipping_method_delivery_time:
+"Do {0}", // By 2013-11-28
+welcome:
+"Dobrodošli",
+back:
+"Nazad",
+order_infos:
+"Informacije o narudžbi",
+generic_error_title:
+"Ups, došlo je do pogreške.",
+promocode_deleted_at_checkout:
+"Popust koji ste upotrijebili dosegao je limit upotrebe dok ste obavljali kupovinu. Ispričavamo se zbog neugodnosti.",
+continue_shopping:
+"Nastavite s kupvinom",
+payment_required_message:
+"Košarica za ovu web stranicu je onemogućena. Ako ste vlasnik ove web stranice, prijavite se na Snipcart kako biste riješili problem.",
+payment_require_title:
+"Košarica za kupnju je onemogućena.",
+configuration_problem:
+"Konfiguracijski problem",
+additionnal_information:
+"Unesite poruku ispod ako želite poslati povratne informacije ili više informacija o tom problemu.",
+send_error:
+"Pošaljite ovaj error vlasniku web stranice",
+message_sent:
+"Poruka je poslana, zahvaljujemo",
+paypalexpress_loading:
+"Pričekajte par sekundi...",
+paypalexpress_cancelled:
+"Otkazali ste kupnju, možete kliknuti na donji gumb kako biste pokušali ponovo ili jednostavno nastavite kupovinu.",
+retry:
+"Pokušajte ponovo",
+error_crawlingfailed_title:
+"Nešto je pošlo po krivu prilikom provjere Vaše narudžbe, ne brinite, nije Vam naplaćeno.",
+error_crawling_unreachable:
+"Proizvod <strong>{0}</strong> je nedostupan <strong>{1}</strong>. Provjerite je li URL proizvoda javno dostupan.",
+error_crawling_product_not_found:
+"Proizvod <strong>{0}</strong> nije pronađen na <strong>{1}</strong>.",
+error_crawling_price_not_found:
+"Proizvod <strong>{0}</strong> nema cijenu na <strong>{1}</strong>, odredite ga sa data-item-price.",
+error_crawling_price_doesnot_match:
+"Proizvod <strong>{0}</strong> cijena <strong>{3}</strong> je <strong>{2}</strong> ali trebala bi biti <strong>{1}</strong>.",
+error_crawlingfailed_title_test:
+"Nešto je pošlo po krivu prilikom provjere Vaše narudžbe, ne brinite, nije Vam naplaćeno. Ova web stranica je trenutno u testnoj fazi.",
+order_completedon:
+"Postavljeno na",
+payment_method_status:
+"Status transakcije",
+payment_method_status_approved:
+"Odobreno",
+order_reference_number:
+"Broj narudžbe",
+order_transaction_amount:
+"Iznos transakcije",
+order_invoice_number:
+"Broj računa",
+order_authorization_code:
+"Autorizacijski kod",
+item_is_being_added:
+"Dodavanje proizvoda u košaru...",
+order_completing_payment:
+"Postavljanje narudžbe...",
+calculating_shipping_fees:
+"Izračunavanje cijene dostave...",
+saving:
+"Spremanje...",
+loading:
+"Učitavanje...",
+free_shipping:
+"Besplatna dostava",
+cart_plans_name:
+"Plan",
+cart_plans_interval:
+"Interval",
+cart_plans_interval_count:
+"Broj",
+cart_plans_quantity:
+"Količina",
+cart_plans_amount:
+"Iznos",
+cart_plans_total:
+"Ukupno",
+payable_now:
+"Trenutno plaćanje",
+to_be_paid_later:
+"Plaćanje kasnije",
+upcoming_payment_for:
+"Pretplata",
+upcoming_payment_date:
+"Datum",
+upcoming_payment_amount:
+"Ukupno",
+upcoming_payment_subtotal:
+"Ukupno",
+upcoming_payment_shipping:
+"Dostava",
+upcoming_payments:
+"Sljedeća naplata",
+order_date:
+"Datum",
+order_status:
+"Status",
+order_total:
+"Ukupno",
+orders_history:
+"Moje narudžbe",
+subscriptions_history_no_subscriptions:
+"Trenutno nemate pretplate.",
+orders_history_no_orders:
+"Trenutno nemate narudžbe.",
+orders_fetching_orders:
+"Učitavanje Vaše narudžbe...",
+user_nav_orders:
+"Moje narudžbe",
+user_nav_subscriptions:
+"Moje pretplate",
+user_nav_cart:
+"Moja košara",
+subscriptions_history:
+"Moje pretplate",
+subscription_name:
+"Ime",
+subscription_amount:
+"Iznos",
+subscription_quantity:
+"Količina",
+subscription_total_amount:
+"Ukupno",
+subscriptions_fetching_subscriptions:
+"Učitavanje pretplate...",
+subscriptions_fetching_details:
+"Učitavanje detalja pretplate...",
+subscription_no_invoices:
+"Ova pretplata trenutno nema računa.",
+subscription_plan_name:
+"Planirano ime",
+subscription_plan_interval:
+"Planirani interval",
+subscription_plan_interval_count:
+"Broj planiranih intervala",
+subscription_invoice_number:
+"Broj",
+subscription_invoice_date:
+"Datum",
+subscription_invoice_amount:
+"Iznos",
+subscription_invoice_item:
+"Proizvod",
+subscription_invoice_info:
+"Informacije računa",
+subscription_invoice_details:
+"Detalji računa",
+subscription_invoice_fetching_details:
+"Učitavanje detalja računa...",
+subscription_invoices_loading:
+"Učitavanje pretplatnih računa...",
+subscription_invoices_list:
+"Povijest računa",
+item_out_of_stock:
+"Žao nam je, ovog proizvoda nema na skladištu.",
+click_here_to_remove_it:
+"Kliknite ovdje kako bi uklonili proizvod iz košare.",
+outofstock_notification:
+"Žao nam je, za vrijeme Vaše kupovine nestalo je zaliha nekih proizvoda koji su u Vašoj košarici. Molim Vas ponovo pregledajte Vašu narudžbu. Ne brinite, Vaša kupvina nije naplaćena.",
+plan_amount_per_day:
+"{0} / dan",
+plan_amount_per_week:
+"{0} / tjedan",
+plan_amount_per_month:
+"{0} / mjesec",
+plan_amount_per_year:
+"{0} / godina",
+plan_amount_per_day_plural:
+"{0} / {1} dana",
+plan_amount_per_week_plural:
+"{0} / {1} tjedana",
+plan_amount_per_month_plural:
+"{0} / {1} mjeseci",
+plan_amount_per_year_plural:
+"{0} / {1} godina",
+plan_days_of_trial:
+"{0} probni dana",
+subscription_cancel_button:
+"Otkaži",
+subscription_pause_button:
+"Pauza",
+subscription_cancel_confirmation:
+"Sigurni ste da želite otkazati pretplatu?",
+subscription_cancel_success:
+"Vaša pretplata je uspješno otkazana.",
+subscription_cancelled_notice:
+"Ova pretplata više nije aktivna, otkazana je od {0}.",
+subscription_cancelledon:
+"Otkazano od",
+paypal_express_checkout_link:
+"Platite Paypal-om",
+paypal_express_checkout_title:
+"Platite Paypal-om",
+paypal_express_checkout_explanation:
+"Kliknite ovaj gumb ako želite izvršiti uplatu izravno putem usluge PayPal-a.",
+notifications_item_not_added_due_to_max_quantity:
+"Ovaj proizvod nije dodan, trenutno ste dosegli maksimalnu količinu proizvoda u košari.",
+error_impossible_to_calculate_taxes:
+"Nismo bili u mogućnosti izračunati porez za ovaj proizvod, to može biti zbog nevažeće adrese, provjerite jeste li unijeli važeću adresu sa ispravnim poštanskim brojem.",
+have_a_promocode_question:
+"Promotivni kod?",
+order_totals_computing:
+"Izračunavanje poreza...",
+order_validation_failed:
+"Nismo uspjeli potvrditi vašu narudžbu. Izgleda da su se neke cijene proizvoda promijenile od trenutka kada ste ih dodali u košaricu. Provjerite narudžbu i pokušajte ponovno.",
+item_invalid_must_remove:
+"Izgleda da proizvod više nije dostupan. Molimo Vas kontaktirajte administratora kako bi se riješio problem budući da može biti problem sa konfiguracijom. Nastavljajući kupnju, ova stavka biti će uklonjena iz vaše košarice.",
+accept_cart_changes:
+"Prihvatite promjene i nastavite",
+payment_failed_text:
+"Ispričavamo se; nismo bili u mogućnosti izvršiti naplatu. Možete nastaviti s kupnjom ili pokušati ponovno pomoću gumba u nastavku.",
+payment_method_willbepaidlater:
+"Odgođeno",
+payment_method_paypal:
+"Paypal",
+payment_method_none:
+"Nitijedan",
+payment_method_sofort:
+"Odmah",
+payment_method_ideal:
+"Idealno",
+payment_method_mistercash:
+"Gotovina",
+payment_method_banktransfer:
+"Bankovni transfer",
+payment_method_directdebit:
+"Izravno zaduženje",
+payment_method_belfius:
+"Belfius",
+payment_method_bitcoin:
+"Bitcoin",
+payment_method_podiumcadeaukaart:
+"Podium Cadeau Kaart",
+payment_method_paysafecard:
+"PaySafe card",
+payment_method_bancontact:
+"Bancontact",
+payment_method_creditcard:
+"Kreditna kartica",
+error_item_stock_exceeded:
+"Oprostite! Vaš zahtjev premašuje dostupnu zalihu za ovaj proizvod.",
+error_item_out_of_stock_text:
+"Oprostite! Izgleda da proizvod '{0}' nije više dostupan. Predlažemo da dostupnost provjerite kasnije ili uklonite proizvod kako biste nastavili.",
+item_out_of_stock_with_variant:
+"Oprostite! Izgleda da varijacija ovog proizvoda više nije dostupna. Molim Vas odaberite drugu varijaciju ili će ovaj proizvod biti uklonjen iz košarice.",
+order_status_processed:
+"Obrađeno",
+order_status_disputed:
+"Zaustavljeno",
+order_status_shipped:
+"Dostavljeno",
+order_status_delivered:
+"Dostavljeno",
+order_status_pending:
+"U tijeku",
+order_status_cancelled:
+"Otkazano",
+something_went_wrong_while_fetching_rates:
+"Nismo bili u mogućnosti naći metodu dostave za Vašu adresu. Kontaktirajte nas direktno ukoliko trebate pomoći oko dostave.",
+error_must_select_shipping_rate:
+"Morate označiti metodu dostave kako bi nastavili.",
+back_to_orders:
+"Natrag na narudžbe",
+manage_subscriptions:
+"Upravljanje pretplatom",
+back_to_subscriptions_list:
+"Natrag na listu pretplate",
+back_to_subscription_details:
+"Natrag na detalje pretplate",
+discount_remove_confirmation_msg:
+"Sigurni ste da želite ukloniti popust?",
+digital_files_not_found:
+"Jedna ili više digitalnih datoteka ne postoje, kontaktirajte vlasnika trgovine za više detalja.",
+digital_files_download_button:
+"Preuzmite datoteku",
+notifications_item_not_modified_due_to_min_quantity:
+"Oprostite, morate naručiti minimalno {0} proizvoda.",
+subscription_resume_buttom:
+"Nastavak",
+subscription_summary:
+"Sažetak",
+subscription_notifications_paused:
+"Ova je pretplata trenutno pauzirana, možete ga nastaviti klikom na gumb Nastavak.",
+subscription_notifications_pause_confirm:
+"Sigurni ste da želite pauzirati pretplatu?",
+subscription_notifications_resume_confirm:
+"Sigurni ste da želite nastaviti pretplatu?",
+subscription_status_canceled:
+"Otkazano",
+subscription_status_paused:
+"Pauzirano",
+refund_line:
+"Refundirano",
+stripe_invalid_number:
+"Broj kartice nije valjani broj kreditne kartice.",
+stripe_invalid_expiry_month:
+"Mjesec isteka kartice je nevažeći.",
+stripe_invalid_expiry_year:
+"Godina isteka kartice je nevažeća.",
+stripe_invalid_cvc:
+"Sigurnosni kod kartice je nevažeći.",
+stripe_invalid_swipe_data:
+"Kartica je nevažeća.",
+stripe_incorrect_number:
+"Broj kartice je nevažeći.",
+stripe_expired_card:
+"Kartica je istekla.",
+stripe_incorrect_cvc:
+"Sigurnosni broj kartice je nevažeći.",
+stripe_incorrect_zip:
+"Provjera zip koda na kartici nije uspjela.",
+stripe_card_declined:
+"Kartica je odbijena.",
+stripe_missing:
+"Ne postoji kartica na klijentu koji se naplaćuje.",
+stripe_processing_error:
+"Došlo je do pogreške tijekom obrade kartice.",
+"profile_credit_card":
+"Kreditna kartica",
+"profile_current_credit_card":
+"Vaša trenutna kreditna kartica završava {0} i isjeće on {1} / {2}.",
+"profile_update_card":
+"Stisnite ovdje kako bi ažurirali",
+"profile_saved_successfully":
+"Vaš je profil uspješno spremljen.",
+"profile_loading":
+"Dobivanje informacija o vašem profilu...",
+"profile_saving":
+"Spremanje vašeg profila...",
+"profile_billing_address":
+"Adresa naplate",
+"profile_shipping_address":
+"Adresa za dostavu",
+"profile_edit_linktitle":
+"Stisnite ovdje kako bi uredili profil.",
+"feature_not_supported_subscriptions": 
+"Nažalost, pretplate nisu dostupne s vašim gatewayom za plaćanje. Ova se stavka ne može dodati.",
+"profile_title": 
+"Profil",
+profile_edit_link:
+"{0}",
+square_field_cardnumber:
+"Broj kartice",
+square_field_cvv:
+"CVC",
+square_field_postalcode:
+"Poštanski broj",
+square_field_expirationdate:
+"Rok trajanja",
+square_validation_error:
+"{0} je nevažeći",
+square_missing_card_data:
+"Jedno ili više polja s podacima o kartici nije ispunjeno u obrascu za plaćanje.",
+square_unknown:
+"Tijekom provjere kartice došlo je do nepoznate pogreške. Molimo pokušajte ponovo kasnije.",
+square_error_payment_processing:
+"Došlo je do neočekivane pogreške prilikom obrade plaćanja. Pokušajte ponovno kasnije",
+square_error_payment_method:
+"Provjera kreditne kartice nije uspjela. Pregledajte pružene informacije."
+});
