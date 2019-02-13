57101c596f01c6da0e966ba28d57fc8593db6c85
Banji55 <47573884+Banji55@users.noreply.github.com>
2019-02-13 21:28:09 +0100

    Add some locales

---
+++ Add nb-NO.js
@@ -0,0 +1,318 @@
+Snipcart.execute('registerLocale', 'nb-NO', {
+yes:
+"Ja",
+no:
+"Nei",
+print:
+"Skriv ut",
+download_as_pdf:
+"Last ned som PDF",
+checkout:
+"Til kassen",
+close:
+"Lukk",
+name:
+"Navn",
+company_name:
+"Firmanavn",
+share_by_email:
+"Del via e-post",
+email:
+"E-post",
+password:
+"Passord",
+confirm_password:
+"Bekreft passord",
+ok:
+"Ok",
+send:
+"Send",
+address_1:
+"Adresse",
+address_2:
+"Adresse 2",
+city:
+"Poststed",
+postal_code:
+"Postnummer",
+phone:
+"Telefonnummer",
+previous:
+"Tilbake",
+next:
+"Neste",
+finalize:
+"Kjop",
+country:
+"Land",
+subtotal:
+"Totalt",
+rebate:
+"Rabatt",
+apply_promo_code:
+"Rabattkode",
+my_cart:
+"Handlekurv",
+continue_shopping:
+"Fortsett a handle",
+my_cart_content:
+"Handlekurv",
+shipping_method:
+"Frakt",
+payment_method:
+"Bestill og betal",
+confirm_order:
+"Betal og fullfor",
+bill_me_later:
+"Betal senere",
+bill_me_later_explanation:
+"Du far en faktura tilsendt pa e-post",
+promo_code_applied_successfully:
+"Rabattkoden er godkjent.",
+promo_code_is_invalid:
+"Rabattkoden er ugyldig.",
+promo_code_is_expired:
+"Rabattkoden er utlopt.",
+promo_code_code:
+"Har du en rabattkode?",
+promo_code_rate_on_order:
+"Rabatt",
+promo_code_alternate_price:
+"Ny pris",
+total:
+"Totalsum",
+province_state:
+"Fylke",
+billing_address:
+"Betalingsadresse",
+shipping_address:
+"Leveringsadresse",
+payment_informations:
+"Betalingsinformasjon",
+payment_informations_bill_me_later:
+"Betal senere",
+payment_informations_paypalexpress:
+"Betal med Paypal",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Januar",
+months_february:
+"Februar",
+months_march:
+"Mars",
+months_april:
+"April",
+months_may:
+"Mai",
+months_june:
+"Juni",
+months_july:
+"Juli",
+months_august:
+"August",
+months_september:
+"September",
+months_october:
+"Oktober",
+months_november:
+"November",
+months_december:
+"Desember",
+cart_items_table_item:
+"Varer",
+cart_items_table_description:
+"Beskrivelse",
+cart_items_table_quantity:
+"Antall",
+cart_items_table_unit_price:
+"Pris",
+cart_items_table_total_price:
+"Totalt",
+cart_empty_text:
+"Handlekurven er tom.",
+new_account_form_create_new_account:
+"Lag ny bruker",
+new_account_form_create_new_account_action:
+"Lag ny bruker",
+login_form_having_an_account:
+"Logg inn",
+login_form_login_action:
+"Logg inn",
+login_form_forgot_password_action:
+"Glemt passord",
+forgot_password_forgot_your_password:
+"Glemt passord?",
+forgot_password_please_enter_email:
+"Oppgi e-postadressen din i feltet under, sa sender vi deg nytt passord.",
+forgot_password_success_email_sent:
+"E-posten med nytt passord er na sendt",
+forgot_password_email_sent_message:
+"Du vil na motta en e-post med instrukser for hvordan du kan opprette et nytt passord.",
+login_checkout_as_guest:
+"Bestill som gjest",
+login_checkout_as_guest_notice:
+"Du kan velge om du vil lage en bruker i slutten av prosessen.",
+shipping_address_same_as_billing:
+"Bruk samme adresse til levering",
+shipping_method_method_name:
+"Leveringsmate",
+shipping_method_shipping_price:
+"Fraktkostnad",
+shipping_method_failure_message:
+"Denne fraktmaten kan ikke benyttes til din adresse. Vennligst sjekk at adressen du har skrevet inn er korrekt.",
+shipping_method_failure_click_here_to_edit:
+"Klikk her for a endre leveringsadresse",
+payment_method_card_holder:
+"Navn pa kortholder",
+payment_method_card_type:
+"Korttype",
+payment_method_card_number:
+"Kortnummer",
+payment_method_card_cvc:
+"CVC",
+payment_method_card_exp_month:
+"Maned",
+payment_method_card_exp_year:
+"Ar",
+payment_method_cvc_infos:
+"For a sorge for at du er eier av kortet krever vi at du oppgir en 3-sifret sikkerhetskode som du finner pa baksiden av kortet ditt.",
+create_an_account:
+"Lag bruker",
+why_create_account:
+"Registrer en konto for a gjore bestillingen raskere neste gang!",
+reset_password:
+"Nullstill passord",
+reset_password_success:
+"Ditt passord er na nullstilt",
+reset_password_changed:
+"Ditt passord er na endret.",
+reset_password_click_here_to_login:
+"Klikk her for a logge inn",
+thankyou_message:
+"Takk for at du bestilte hos oss.",
+thankyou_submessage:
+"Du vil na motta en e-post med ordrebekreftelsen!",
+account_created_successfully:
+"Profil oprettet.",
+account_created_successfully_message:
+"Du har opprettet en profil!",
+errors_required:
+"Feltet ma fylles ut",
+errors_email_must_be_unique:
+"E-postadressen er allerede i bruk",
+errors_both_password_must_match:
+"Passordene stemmer ikke overens",
+errors_email_must_be_valid:
+"Tast inn en ekte e-postadresse",
+errors_email_does_not_match_any_existing_user:
+"Denne e-posten har ingen konto",
+errors_email_does_not_match_reset_password_request:
+"E-posten stemmer ikke",
+errors_reset_password_token_expired:
+"Be om nytt passord igjen.",
+errors_invalid_authentication_infos:
+"E-post og/eller passordet er feil",
+error_payment_items_empty:
+"Vi kan for oyeblikket ikke gjennomfore din bestilling. Prov igjen om et oyeblikk!",
+error_payment_items_are_invalid:
+"Det er ikke mulig a gjennomfore din bestilling. En av varerne har en ugyldig pris. Ta kontakt med kundestotte sa fort som mulig!",
+error_crawling_failed:
+"Det er ikke mulig a gjennomfore din bestilling. Vennligst kontakt kundestotte.",
+powered_by:
+"Drevet og sikret av",
+promocode_rate_format:
+"{0}% rabatt pa din ordre",
+promocode_amount_format:
+"{0} rabatt pa din ordre",
+shipping_method_business_days:
+"{0} virkedager",
+shipping_method_business_day:
+"{0} virkedag",
+shipping_method_delivery_time:
+"Til {0}", // By 2013-11-28
+welcome:
+"Velkommen",
+back:
+"Tilbake",
+order_infos:
+"Ordreinformasjon",
+generic_error_title:
+"Oops, en feil oppstod.",
+promocode_deleted_at_checkout:
+"Denne rabattkoden er allerede brukt.",
+continue_shopping:
+"Fortsett a handle",
+payment_required_message:
+"Betalingssystemet er ikke tilgjengelig. Kontakt kundestotte for hjelp!",
+payment_require_title:
+"Betalingssystemet er deaktivert.",
+configuration_problem:
+"Konfigurasjonsproblem",
+additionnal_information:
+"Skriv inn en beskjed med tilbakemelding om problemet.",
+send_error:
+"Send meldingen til eieren av nettstedet",
+message_sent:
+"Takk for beskjeden",
+paypalexpress_loading:
+"Du omdirigeres til PayPal for a gjennomfore betalingen",
+paypalexpress_cancelled:
+"Ordren er annulert.",
+retry:
+"Prov igen",
+error_crawlingfailed_title:
+"Vi klarte ikke a gjennomfore din bestilling.",
+error_crawling_unreachable:
+"Varen <strong>{0}</strong> er ikke tilgjengelig <strong>{1}</strong>. Se om ditt produkt URL er tilgjengelig.",
+error_crawling_product_not_found:
+"varen <strong>{0}</strong> ble ikke funnet <strong>{1}</strong>.",
+error_crawling_price_not_found:
+"Varen <strong>{0}</strong> har ikke fatt spesifert en pris <strong>{1}</strong>, spesifisere prisen med data-item-price.",
+error_crawling_price_doesnot_match:
+"Varens <strong>{0}</strong> pris <strong>{3}</strong> er <strong>{2}</strong> men burde vare <strong>{1}</strong>.",
+error_crawlingfailed_title_test:
+"Det har oppstatt en feil i ordren din! Hjemmesiden er i test-modus, og du har derfor ikke blitt fratrukket belopet fra din konto.",
+order_completedon:
+"Ordren er bekreftet",
+payment_method_status:
+"Status pa betaling",
+payment_method_status_approved:
+"Godkjent",
+order_reference_number:
+"Referansenummer",
+order_transaction_amount:
+"Transaksjonsbelop",
+order_invoice_number:
+"Fakturanummer",
+order_authorization_code:
+"Autorisasjonskode",
+cart_plans_interval:
+"Intervall",
+cart_plans_interval_count:
+"Antall",
+subscription_plan_interval:
+"Abonnementsintervall",
+subscription_plan_interval_count:
+"Antall abonnementsintervall",
+plan_amount_per_day:
+"{0} / dag",
+plan_amount_per_week:
+"{0} / uke",
+plan_amount_per_month:
+"{0} / maned",
+plan_amount_per_year:
+"{0} / ar",
+plan_amount_per_day_plural:
+"{0} / {1} dager",
+plan_amount_per_week_plural:
+"{0} / {1} uker",
+plan_amount_per_month_plural:
+"{0} / {1} maneder",
+plan_amount_per_year_plural:
+"{0} / {1} ar"
+});
