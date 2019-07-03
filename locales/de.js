13b47e0fd2b8e64fb34dd185b60950b98937c427
Dominique Eugster <dominique@duotones.ch>
2019-07-03 18:21:13 +0200

    de: rebased translation on 'en.js' - reordered keys - added missing strings - updated strings, that didn't reflect 'en.js' anymore

--- locales/de.js
+++ locales/de.js
@@ -1,88 +1,4 @@
 Snipcart.execute('registerLocale', 'de', {
-first_name:
-"Vorname",
-last_name:
-"Nachname",
-bill_me_later_action:
-"Später bezahlen",
-pay_via_mollie:
-"Mit mollie bezahlen",
-pay_now_via_mollie:
-"Jetzt mit mollie bezahlen",
-pay_via_mollie_explanation:
-"Sie werden zu einer Liste mit verfügbaren Zahlungsoptionen weitergeleitet.",
-payment_informations_no_payment_required:
-"Keine Zahlung erforderlich",
-payment_status:
-"Zahlungsstatus",
-errors_passwords_dont_match:
-"Passwörter stimmen nicht überein",
-error_discounts_have_expired:
-"Rabatte sind abgelaufen",
-have_a_promocode_question:
-"Haben Sie einen Gutschein-Code?",
-order_totals_computing:
-"Berechne Summe ...",
-order_validation_failed:
-"Wir konnten Ihre Bestellung nicht bestätigen. Anscheinend wurden Produktpreise geändert, seit Sie den Artikel in den Warenkorb gelegt haben. Bitte überprüfen Sie Ihre Bestellung und versuchen Sie es erneut.",
-item_invalid_must_remove:
-"Anscheinend ist dieser Artikel nicht mehr verfügbar. Kontaktieren Sie den Shop-Betreiber und informieren Sie ihn über dieses Problem. Fahren Sie fort, um den Artikel aus dem Warenkorb zu entfernen.",
-accept_cart_changes:
-"Akzeptiere Korrekturen und fahre fort",
-payment_failed_text:
-"Sorry, wir konnten Ihre Bezahlung nicht verarbeiten. Sie können weiter einkaufen oder es noch einmal versuchen.",
-payment_method_willbepaidlater:
-"Wird später bezahlt",
-payment_method_paypal:
-"PayPal",
-payment_method_none:
-"Keine Zahlungsmethode",
-payment_method_sofort:
-"Sofort",
-payment_method_ideal:
-"Ideal",
-payment_method_mistercash:
-"Mistercash",
-payment_method_banktransfer:
-"Banktransfer",
-payment_method_directdebit:
-"Directdebit",
-payment_method_belfius:
-"Belfius",
-payment_method_bitcoin:
-"Bitcoin",
-payment_method_podiumcadeaukaart:
-"Podium Cadeau Kaart",
-payment_method_paysafecard:
-"PaySafe card",
-payment_method_bancontact:
-"Bancontact",
-payment_method_creditcard:
-"Kreditkarte",
-error_item_stock_exceeded:
-"Sorry! Die gewünschte Artikelanzahl ist nicht mehr auf Lager.",
-error_item_out_of_stock_text:
-"Sorry! Dieser Artikel ist nicht mehr auf Lager. Bitte versuchen Sie es später wieder.",
-item_out_of_stock_with_variant:
-"Sorry! Diese Artikelvariante ist nicht mehr auf Lager. Bitte wählen Sie eine andere Variante aus, oder dieser Artikel wird aus dem Warenkorb entfernt.",
-order_status_processed:
-"Verarbeitet",
-order_status_disputed:
-"Reklamiert",
-order_status_shipped:
-"Versendet",
-order_status_delivered:
-"Geliefert",
-order_status_pending:
-"Steht aus",
-order_status_cancelled:
-"Abgebrochen",
-something_went_wrong_while_fetching_rates:
-"Beim Berechnen des Versandpreises für Ihre Bestellung ist ein Fehler aufgetreten. Bitte überprüfen Sie die angegebene Versandadresse. Der Fehler ist:",
-error_must_select_shipping_rate:
-"Sie müssen eine Versandmethode auswählen, um fortzufahren.",
-back_to_orders:
-"Zurück zu den Bestellungen",
 yes:
 "Ja",
 no:
@@ -92,11 +8,15 @@ print:
 download_as_pdf:
 "Als PDF herunterladen",
 checkout:
-"Zur Kasse",
+"Kasse",
 close:
-"Schließen",
+"Schliessen",
+first_name:
+"Vorname",
 name:
 "Name",
+last_name:
+"Nachname",
 company_name:
 "Firma",
 share_by_email:
@@ -149,8 +69,16 @@ confirm_order:
 "Kauf bestätigen",
 bill_me_later:
 "Später bezahlen",
+bill_me_later_action:
+"Später bezahlen",
 bill_me_later_explanation:
 "Eine Rechnung wird Ihnen via E-Mail zugestellt.",
+pay_via_mollie:
+"Mit mollie bezahlen",
+pay_now_via_mollie:
+"Jetzt mit mollie bezahlen",
+pay_via_mollie_explanation:
+"Sie werden zu einer Liste mit verfügbaren Zahlungsoptionen weitergeleitet.",
 promo_code_applied_successfully:
 "Ihr Gutschein wurde erfolgreich eingelöst.",
 promo_code_is_invalid:
@@ -159,8 +87,6 @@ promo_code_is_expired:
 "Dieser Gutscheincode ist abgelaufen.",
 promo_code_code:
 "Haben Sie einen Gutschein?",
-discount_remove_confirmation_msg:
-"Möchten Sie den Gutschein entfernen?",
 promo_code_rate_on_order:
 "Rabatt auf Ihre Bestellung",
 promo_code_alternate_price:
@@ -179,6 +105,8 @@ payment_informations:
 "Zahlung",
 payment_informations_bill_me_later:
 "Ich bezahle später",
+payment_informations_no_payment_required:
+"Keine Zahlung erforderlich",
 payment_informations_paypalexpress:
 "Zahlung mit Paypal",
 credit_card_type_mastercard:
@@ -266,13 +194,19 @@ payment_method_card_number:
 payment_method_card_cvc:
 "CVC",
 payment_method_card_exp_month:
-"Ablauf Monat",
+"Ablaufmonat",
 payment_method_card_exp_year:
-"Ablauf Jahr",
+"Ablaufjahr",
 payment_method_cvc_infos:
-"CVC: die 3-stellige Sicherheits-Zahl auf der Rückseite Ihrer Karte, normalerweise rechts von Ihrer Unterschrift.",
+"CVC: die 3-stellige Sicherheitscode auf der Rückseite Ihrer Karte, normalerweise rechts von Ihrer Unterschrift.",
+payment_method_card_exp_date:
+"Ablaufdatum",
+payment_method_card_postal_code:
+"PLZ",
+payment_status:
+"Zahlungsstatus",
 create_an_account:
-"Konto erstellen",
+"Ein Konto erstellen",
 why_create_account:
 "Erstellen Sie ein Konto, damit Sie bei Ihrem nächsten Einkauf Zeit sparen. Geben Sie ein Passwort an, um ein Konto zu erstellen.",
 reset_password:
@@ -293,6 +227,8 @@ account_created_successfully_message:
 "Ihr Konto wurde erfolgreich erstellt, vielen Dank.",
 errors_required:
 "Dies ist ein Pflichtfeld",
+errors_passwords_dont_match:
+"Die beiden Passwörter müssen identisch sein",
 errors_email_must_be_unique:
 "Ein Benutzer mit der gleichen E-Mail-Adresse ist bereits vorhanden.",
 errors_both_password_must_match:
@@ -309,12 +245,18 @@ errors_invalid_authentication_infos:
 "Ungültige Identifizierungsangaben",
 error_payment_items_empty:
 "Ihre Bestellung scheint ungültig zu sein. Bitte laden Sie die Seite neu. Es wurde keine Zahlung ausgelöst.",
+error_email_is_empty:
+"Sieht so aus, als hätten wir ein Problem beim Speichern Ihrer E-Mail-Adresse, bitte geben Sie sie erneut ein und klicken Sie auf Weiter, um fortzufahren.",
 error_payment_items_are_invalid:
 "Wir konnten Ihre Bestellung nicht abschließen. Eines der Produkte in Ihrem Warenkorb scheint einen ungültigen Preis zu haben.",
 error_crawling_failed:
 "Wir konnten Ihre Bestellung nicht abschließen. Ihre Kreditkarte wurde nicht belastet. Bitte versuchen Sie es in wenigen Augenblicken nochmal.",
+error_discounts_have_expired:
+"Leider ist einer der Rabatte, den Sie eingegeben haben abgelaufen. Bitte kontrollieren Sie die untenstehende Bestellung und versuchen Sie es erneut.",
+error_shipping_price_outdated:
+"Die von Ihnen gewählte Versandart ist nicht mehr gültig, bitte wählen Sie eine andere Versandart.",
 powered_by:
-"Powered by",
+"Powered and secured by",
 promocode_rate_format:
 "{0}% Rabatt auf Ihre Bestellung",
 promocode_amount_format:
@@ -322,9 +264,9 @@ promocode_amount_format:
 shipping_method_business_days:
 "{0} Arbeitstage",
 shipping_method_business_day:
-"{0} Arbeitstag",
+"{0} Arbeitstage",
 shipping_method_delivery_time:
-"Bis {0}",
+"Bis {0}", // By 2013-11-28
 welcome:
 "Willkommen",
 back:
@@ -350,7 +292,7 @@ send_error:
 message_sent:
 "Die Nachricht wurde gesendet. Danke!",
 paypalexpress_loading:
-"Sie werden gleich weitergeleitet, bitte warten...",
+"Sie werden gleich weitergeleitet, bitte warten …",
 paypalexpress_cancelled:
 "Sie haben die Transaktion abgebrochen. Wenn Sie es nochmals versuchen wollen, klicken Sie auf den Knopf.",
 retry:
@@ -407,6 +349,8 @@ cart_plans_total:
 "Summe",
 payable_now:
 "Jetzt fällig",
+to_be_paid_later:
+"Ist zu einem späteren Zeitpunkt zu bezahlen",
 upcoming_payment_for:
 "Abonnement",
 upcoming_payment_date:
@@ -415,6 +359,8 @@ upcoming_payment_amount:
 "Summe",
 upcoming_payment_subtotal:
 "Zwischensumme",
+upcoming_payment_shipping:
+"Versandkosten",
 upcoming_payments:
 "Nächste Zahlungen",
 order_date:
@@ -424,7 +370,9 @@ order_status:
 order_total:
 "Summe",
 orders_history:
-"Bestell-Historie",
+"Bestellverlauf",
+subscriptions_history_no_subscriptions:
+"Sie haben bisher noch keine Abonnements.",
 orders_history_no_orders:
 "Es wurden bisher keine Bestellungen durchgeführt.",
 orders_fetching_orders:
@@ -436,7 +384,7 @@ user_nav_subscriptions:
 user_nav_cart:
 "Mein Warenkorb",
 subscriptions_history:
-"Abonnement-Historie",
+"Abonnementsverlauf",
 subscription_name:
 "Name",
 subscription_amount:
@@ -452,11 +400,11 @@ subscriptions_fetching_details:
 subscription_no_invoices:
 "Für dieses Abonnement liegen noch keine Rechnungen vor.",
 subscription_plan_name:
-"Plan Name",
+"Abonnementsname",
 subscription_plan_interval:
-"Plan Interval",
+"Abonnementsintervall",
 subscription_plan_interval_count:
-"Plan-Intervall Graf",
+"Anzahl Abonnementsintervalle",
 subscription_invoice_number:
 "Nummer",
 subscription_invoice_date:
@@ -474,13 +422,13 @@ subscription_invoice_fetching_details:
 subscription_invoices_loading:
 "Lade Abonnement-Rechnungen …",
 subscription_invoices_list:
-"Rechnungs-Historie",
+"Rechnungsverlauf",
 item_out_of_stock:
 "Es tut uns leid, dieser Artikel ist momentan nicht vorrätig.",
 click_here_to_remove_it:
 "Klicken Sie hier, um den Artikel aus dem Warenkorb zu entfernen.",
 outofstock_notification:
-"Es tut uns leid, Artikel in Ihrem Warenkorb sind nicht vorrätig. Bitte überprüfen Sie Ihre Bestellung. Keine Angst, es wurde noch nichts berechnet.",
+"Es tut uns leid, einige Artikel in Ihrem Warenkorb sind nicht mehr vorrätig. Bitte überprüfen Sie Ihre Bestellung. Keine Sorge, Ihre Karte wurde nicht belastet.",
 plan_amount_per_day:
 "{0} / Tag",
 plan_amount_per_week:
@@ -494,13 +442,15 @@ plan_amount_per_day_plural:
 plan_amount_per_week_plural:
 "{0} / {1} Wochen",
 plan_amount_per_month_plural:
-"{0} / {1} Monate",
+"{0} / {1} Monaten",
 plan_amount_per_year_plural:
-"{0} / {1} Jahre",
+"{0} / {1} Jahren",
 plan_days_of_trial:
 "{0} Tage Probe",
 subscription_cancel_button:
-"Dieses Abonnement kündigen",
+"Abonnement kündigen",
+subscription_pause_button:
+"Abonnement unterbrechen",
 subscription_cancel_confirmation:
 "Sind Sie sicher, dass Sie Ihr Abonnement kündigen möchten?",
 subscription_cancel_success:
@@ -516,7 +466,169 @@ paypal_express_checkout_title:
 paypal_express_checkout_explanation:
 "Klicken Sie auf diesen Button, wenn Sie die Bezahlung direkt über PayPal abwickeln möchten.",
 notifications_item_not_added_due_to_max_quantity:
-"Der Artikel wurde nicht hinzugefügt, weil Sie die maximal zulässige Anzahl überschreiten würden.",
+"Das Produkt wurde nicht hinzugefügt, Sie haben bereits die maximale Menge in Ihrem Warenkorb.",
 error_impossible_to_calculate_taxes:
-"Wir konnten keine Steuer für diese Bestellung berechnen. Das kann an einer ungültigen Adresse liegen. Bitte stellen Sie sicher, dass Ihre Adresse eine gültige Postleitzahl enthält."
+"Wir konnten keine Steuer für diese Bestellung berechnen. Das kann an einer ungültigen Adresse liegen. Bitte stellen Sie sicher, dass Ihre Adresse eine gültige Postleitzahl enthält.",
+have_a_promocode_question:
+"Haben Sie einen Gutschein-Code?",
+order_totals_computing:
+"Steuern werden berechnet …",
+order_validation_failed:
+"Wir konnten Ihre Bestellung nicht bestätigen. Anscheinend wurden Produktpreise geändert, seit Sie den Artikel in den Warenkorb gelegt haben. Bitte überprüfen Sie Ihre Bestellung und versuchen Sie es erneut.",
+item_invalid_must_remove:
+"Anscheinend ist dieser Artikel nicht mehr verfügbar. Möglicherweise müssen Sie sich an den Händler wenden, um eine Lösung zu erhalten, da es sich um ein Konfigurationsproblem handeln kann. Fahren Sie fort, wird der Artikel aus dem Warenkorb entfernt.",
+accept_cart_changes:
+"Änderungen übernehmen und fortfahren",
+payment_failed_text:
+"Es tut uns leid, aber wir konnten Ihre Zahlung nicht bearbeiten. Sie können mit dem Einkauf fortfahren oder es mit den untenstehenden Buttons erneut versuchen.",
+payment_method_willbepaidlater:
+"Wird später bezahlt",
+payment_method_paypal:
+"Paypal",
+payment_method_none:
+"Keine Zahlungsmethode",
+payment_method_sofort:
+"Sofort",
+payment_method_ideal:
+"Ideal",
+payment_method_mistercash:
+"Mister Cash",
+payment_method_banktransfer:
+"Banktransfer",
+payment_method_directdebit:
+"Lastschrift",
+payment_method_belfius:
+"Belfius",
+payment_method_bitcoin:
+"Bitcoin",
+payment_method_podiumcadeaukaart:
+"Podium Cadeau Kaart",
+payment_method_paysafecard:
+"PaySafe card",
+payment_method_bancontact:
+"Bancontact",
+payment_method_creditcard:
+"Kreditkarte",
+error_item_stock_exceeded:
+"Es tut uns leid, aber die gewünschte Artikelanzahl ist nicht mehr auf Lager.",
+error_item_out_of_stock_text:
+"Entschuldigung! Es sieht so aus, als ob das Produkt '{0}' nicht mehr verfügbar ist. Bitte versuchen Sie es später wieder oder entfernen sie das Produkt, um fortzufahren.",
+item_out_of_stock_with_variant:
+"Entschuldigung! Es sieht so aus, als ob diese Variante des Produkts nicht mehr verfügbar ist. Bitte wählen Sie eine andere Variante aus, sonst wird dieser Artikel aus dem Warenkorb entfernt.",
+order_status_processed:
+"Verarbeitet",
+order_status_disputed:
+"Reklamiert",
+order_status_shipped:
+"Versendet",
+order_status_delivered:
+"Geliefert",
+order_status_pending:
+"Ausstehend",
+order_status_cancelled:
+"Abgebrochen",
+something_went_wrong_while_fetching_rates:
+"Für die von Ihnen angegebene Adresse konnten wir keine Versandart finden. Kontaktieren Sie uns direkt, wenn Sie diesbezüglich Hilfe benötigen.",
+error_must_select_shipping_rate:
+"Sie müssen eine Versandmethode auswählen, um fortzufahren.",
+back_to_orders:
+"Zurück zu den Bestellungen",
+manage_subscriptions:
+"Abonnements verwalten",
+back_to_subscriptions_list:
+"Zurück zur Abonnementsliste",
+back_to_subscription_details:
+"Zurück zu den Abonnementdetails",
+discount_remove_confirmation_msg:
+"Möchten Sie den Gutschein wirklich entfernen?",
+digital_files_not_found:
+"Eine oder mehrere digitale Dateien existieren nicht, wenden Sie sich für weitere Informationen an den Shop-Betreiber.",
+digital_files_download_button:
+"Datei herunterladen",
+notifications_item_not_modified_due_to_min_quantity:
+"Sie müssen mindestens {0} dieses Artikels bestellen.",
+subscription_resume_buttom:
+"Wieder aufnehmen",
+subscription_summary:
+"Zusammenfassung",
+subscription_notifications_paused:
+"Dieses Abonnement ist momentan unterbrochen, Sie können es wieder aufnehmen, indem Sie auf Fortsetzen klicken.",
+subscription_notifications_pause_confirm:
+"Sind Sie sicher, dass Sie Ihr Abonnement unterbrechen möchten?",
+subscription_notifications_resume_confirm:
+"Sind Sie sicher, dass Sie dieses Abonnement wieder aufnehmen möchten?",
+subscription_status_canceled:
+"Annuliert",
+subscription_status_paused:
+"Unterbrochen",
+refund_line:
+"Rückerstattet",
+stripe_invalid_number:
+"Die Kartennummer ist keine gültige Kreditkartennummer.",
+stripe_invalid_expiry_month:
+"Der Ablaufmonat der Karte ist ungültig.",
+stripe_invalid_expiry_year:
+"Das Ablaufjahr der Karte ist ungültig.",
+stripe_invalid_cvc:
+"Der Sicherheitscode (CVC) der Karte ist ungültig.",
+stripe_invalid_swipe_data:
+"Die Magnetstreifendaten der Karte sind ungültig.",
+stripe_incorrect_number:
+"Die Kartennummer ist falsch.",
+stripe_expired_card:
+"Die Karte ist abgelaufen.",
+stripe_incorrect_cvc:
+"Der Sicherheitscode (CVC) der Karte ist falsch.",
+stripe_incorrect_zip:
+"Die PLZ der Karte konnte nicht validiert werden.",
+stripe_card_declined:
+"Die Karte wurde abgelehnt.",
+stripe_missing:
+"Es ist keine Kreditkarte für diesen Kunden registriert.",
+stripe_processing_error:
+"Bei der Verarbeitung der Karte ist ein Fehler aufgetreten.",
+profile_credit_card:
+"Kreditkarte",
+profile_current_credit_card:
+"Ihre aktuelle Kreditkarte endet mit {0} und verfällt im {1} / {2}.",
+profile_update_card:
+"Klicken Sie hier, um sie zu aktualisieren",
+profile_saved_successfully:
+"Ihr Profil wurde erfolgreich gespeichert.",
+profile_loading:
+"Ihre Profilinformationen werden abgerufen …",
+profile_saving:
+"Ihr Profil wird gespeichert …",
+profile_billing_address:
+"Rechnungsadresse",
+profile_shipping_address:
+"Lieferadresse",
+profile_edit_linktitle:
+"Klicken Sie hier, um Ihr Profil zu bearbeiten.",
+feature_not_supported_subscriptions: 
+"Es tut uns leid, aber Abonnements sind mit Ihrer Zahlungsart nicht möglich. Dieses Element kann nicht hinzugefügt werden.",
+profile_title: 
+"Profil",
+profile_edit_link:
+"{0}",
+square_field_cardnumber:
+"Kartennummer",
+square_field_cvv:
+"CVC",
+square_field_postalcode:
+"PLZ",
+square_field_expirationdate:
+"Ablaufdatum",
+square_validation_error:
+"{0} ist ungültig",
+square_missing_card_data:
+"Ein oder mehrere Kartendatenfelder wurden im Zahlungsformular nicht ausgefüllt.",
+square_unknown:
+"Bei der Kreditkartenprüfung ist ein unbekannter Fehler aufgetreten. Bitte versuchen Sie es später noch einmal.",
+square_error_payment_processing:
+"Bei der Verarbeitung der Zahlung ist ein unerwarteter Fehler aufgetreten. Bitte versuchen Sie es später noch einmal.",
+square_error_payment_method:
+"Die Kreditkartenprüfung ist fehlgeschlagen. Bitte überprüfen Sie Ihre Angaben.",
+save:
+"Speichenr"
 });
